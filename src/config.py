28f3c3a061ee9ee5f0a3f6ec98127ddcca18bb1d
Z1pP <maxm_99@mail.ru>
2025-05-09 14:00:13 +0300

    update config

--- src/config.py
+++ src/config.py
@@ -2,12 +2,32 @@ from pydantic import Field
 from pydantic_settings import BaseSettings
 
 
-class Settings(BaseSettings):
-    BOT_TOKEN: str = Field(alias="BOT_TOKEN")
+class DataBaseConfig(BaseSettings):
+    DEV_DATABASE_URL: str = Field(alias="DEV_DATABASE_URL")
+    PROD_DATABSE_URL: str = Field(alias="PROD_DATABSE_URL")
 
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
+        extra = "allow"
+
+
+class Settings(BaseSettings):
+    BOT_TOKEN: str = Field(alias="BOT_TOKEN")
+    IS_DEVELOPMENT: bool = Field(alias="IS_DEVELOPMENT", default=False)
+
+    @property
+    def DATABASE_URL(self) -> str:
+        db_config = DataBaseConfig()
+
+        if self.IS_DEVELOPMENT:
+            return db_config.DEV_DATABASE_URL
+        return db_config.PROD_DATABSE_URL
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+        extra = "allow"
 
 
 settings = Settings()
