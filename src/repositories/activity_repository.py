9cf5581ebb7ce4d41b4aa6711468680c78e9bae4
Z1pP <maxm_99@mail.ru>
2025-05-13 13:14:42 +0300

    fix: correct activity handling in create_simple_activity and create_reply_activity methods

--- src/repositories/activity_repository.py
+++ src/repositories/activity_repository.py
@@ -1,4 +1,4 @@
-from typing import Union
+from sqlalchemy import select
 
 from database.session import async_session
 from models import ChatMessage, MessageReply, ModeratorActivity
@@ -16,9 +16,30 @@ class ActivityRepository:
         )
         async with async_session() as session:
             try:
-                session.add(activity)
+                session.add(new_activity)
                 await session.commit()
-                await session.refresh(activity)
+                await session.refresh(new_activity)
+                return activity
+
+            except Exception as e:
+                print(str(e))
+                await session.rollback()
+                raise e
+
+    async def create_reply_activity(
+        self, message: MessageReply, activity: ModeratorActivity
+    ) -> ModeratorActivity:
+        new_activity = ModeratorActivity(
+            moderator_id=message.user_id,
+            chat_id=message.chat_id,
+            message_id=message.id,
+            reply_to_message_id=message.reply_to_message_id,
+        )
+        async with async_session() as session:
+            try:
+                session.add(new_activity)
+                await session.commit()
+                await session.refresh(new_activity)
                 return activity
             # return activity.id
             except Exception as e:
@@ -30,11 +51,12 @@ class ActivityRepository:
         async with async_session() as session:
             try:
                 return await session.scalar(
-                    ModeratorActivity.select().where(
+                    select(ModeratorActivity).where(
                         ModeratorActivity.moderator_id == user_id,
                         ModeratorActivity.chat_id == chat_id,
                     )
                 )
+
             except Exception as e:
                 print(str(e))
                 return None
