4e1d8b8ba4b3524ca64193bf995a26decd8df83b
Z1pP <maxm_99@mail.ru>
2025-05-15 22:15:03 +0300

    refactor: update create_new_message and create_reply_message methods to use DTOs for better data handling

--- src/repositories/message_repository.py
+++ src/repositories/message_repository.py
@@ -3,7 +3,9 @@ from typing import Optional
 from sqlalchemy import select
 
 from database.session import async_session
-from models import ChatMessage
+from dto.message import CreateMessageDTO
+from dto.message_reply import CreateMessageReplyDTO
+from models import ChatMessage, MessageReply
 
 
 class MessageRepository:
@@ -26,22 +28,39 @@ class MessageRepository:
                 print(str(e))
                 return None
 
-    async def create_new_message(
-        self, user_id: int, chat_id: int, message_id: str, message_type: str, text: str
-    ) -> ChatMessage:
+    async def create_new_message(self, dto: CreateMessageDTO) -> ChatMessage:
         async with async_session() as session:
             try:
                 new_message = ChatMessage(
-                    user_id=user_id,
-                    chat_id=chat_id,
-                    message_id=message_id,
-                    message_type=message_type,
-                    text=text,
+                    user_id=dto.user_id,
+                    chat_id=dto.chat_id,
+                    message_id=dto.message_id,
+                    message_type=dto.message_type,
+                    text=dto.text,
                 )
                 session.add(new_message)
                 await session.commit()
                 await session.refresh(new_message)
                 return new_message
+            except Exception as e:
+                print(str(e))
+                await session.rollback()
+                raise e
+
+    async def create_reply_message(self, dto: CreateMessageReplyDTO) -> MessageReply:
+        async with async_session() as session:
+            try:
+                new_reply = MessageReply(
+                    chat_id=dto.chat_id,
+                    original_message_url=dto.original_message_url,
+                    reply_message_id=dto.reply_message_id,
+                    reply_user_id=dto.reply_user_id,
+                    response_time_seconds=dto.response_time_seconds,
+                )
+                session.add(new_reply)
+                await session.commit()
+                await session.refresh(new_reply)
+                return new_reply
             except Exception as e:
                 print(str(e))
                 await session.rollback()
