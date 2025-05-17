c89ec90d43d9023a402b25019d059abf54cac976
Z1pP <maxm_99@mail.ru>
2025-05-17 19:08:13 +0300

    fix: update REPORT_DAILY command description for clarity

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -8,7 +8,7 @@ class CommandList(str, Enum):
     HELP = "Получить справку по командам"
     ADD_MODERATOR = "Добавить модератора (Пример: /add_moderator @username)"
     REMOVE_MODERATOR = "Удалить модератора (Пример: /remove_moderator @username)"
-    REPORT_DAILY = "Получить отчет о количестве сообщений за день @username"
+    REPORT_DAILY = "Отчет о кол-ве сообщений за период. (Пример: /report_daily 21.04-25.04 @username)"
     REPORT_AVG = "Получить отчет о среднем количестве сообщений за период. Пример: /report_avg 6h"
     REPORT_RESPONSE_TIME = "Получить отчет о времени ответа на сообщения @username"
     REPORT_INACTIVE = "Получить отчет о периодах неактивности модератора @username"
