83f0caa7fee54481d8f41d1c456e6955061dddd5
Z1pP <maxm_99@mail.ru>
2025-05-18 00:42:55 +0300

    feat: implement send_html_message utility for sending formatted messages

---
+++ src/utils/send_message.py
@@ -0,0 +1,10 @@
+from aiogram.enums import ParseMode
+from aiogram.types import Message
+
+
+async def send_html_message(
+    text: str,
+    message: Message,
+    parse_mode: ParseMode = ParseMode.HTML,
+) -> None:
+    await message.answer(text=text, parse_mode=parse_mode)
