90e7d6514c0153a7c32df6cbe905c182ca7f58e7
Z1pP <maxm_99@mail.ru>
2025-05-17 19:08:25 +0300

    feat: include report_daily_router in the router registry

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -2,8 +2,10 @@ from aiogram import Dispatcher
 
 from .group.message_handler import router as message_router
 from .private.commands_handler import router as commands_router
+from .private.report_daily_handler import router as report_daily_router
 
 
 def registry_routers(disptcher: Dispatcher):
     disptcher.include_router(message_router)
     disptcher.include_router(commands_router)
+    disptcher.include_router(report_daily_router)
