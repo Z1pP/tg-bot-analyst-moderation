11871e12209b0c3110f2871a96f07fb4fc866568
Z1pP <maxm_99@mail.ru>
2025-05-17 21:03:31 +0300

    feat: add GetAvgMessageCountUseCase to module exports

--- src/usecases/report/__init__.py
+++ src/usecases/report/__init__.py
@@ -1,5 +1,7 @@
+from .get_avg_message_count_usecase import GetAvgMessageCountUseCase
 from .get_daily_report_usecase import GetDailyReportUseCase
 
 __all__ = [
     "GetDailyReportUseCase",
+    "GetAvgMessageCountUseCase",
 ]
