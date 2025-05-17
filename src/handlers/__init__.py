bf5c3301ad363a78a64de9bef3d8ba111d5437b2
Z1pP <maxm_99@mail.ru>
2025-05-17 21:03:15 +0300

    feat: include avg_messages_count_router in the router registry

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -1,6 +1,7 @@
 from aiogram import Dispatcher
 
 from .group.message_handler import router as message_router
+from .private.avg_messages_count_handler import router as avg_messages_count_router
 from .private.commands_handler import router as commands_router
 from .private.report_daily_handler import router as report_daily_router
 
@@ -9,3 +10,4 @@ def registry_routers(disptcher: Dispatcher):
     disptcher.include_router(message_router)
     disptcher.include_router(commands_router)
     disptcher.include_router(report_daily_router)
+    disptcher.include_router(avg_messages_count_router)
