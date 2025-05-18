f42ec2a310c6a3859f3636d25c546b23f0552b79
Z1pP <maxm_99@mail.ru>
2025-05-18 12:57:44 +0300

    fix: include GetResponseTimeReportUseCase in __all__ exports

--- src/usecases/report/__init__.py
+++ src/usecases/report/__init__.py
@@ -1,7 +1,9 @@
 from .get_avg_message_count_usecase import GetAvgMessageCountUseCase
 from .get_daily_report_usecase import GetDailyReportUseCase
+from .get_response_time_report_usecase import GetResponseTimeReportUseCase
 
 __all__ = [
     "GetDailyReportUseCase",
     "GetAvgMessageCountUseCase",
+    "GetResponseTimeReportUseCase",
 ]
