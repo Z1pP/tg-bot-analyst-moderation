7800312021c4472c2590d3e99409610fef55f897
Z1pP <maxm_99@mail.ru>
2025-05-18 16:19:02 +0300

    refactor: reorganize ChatTypeFilter class for improved readability

--- src/filters/group_filter.py
+++ src/filters/group_filter.py
@@ -12,3 +12,11 @@ class GroupTypeFilter(Filter):
 
     async def __call__(self, message: Message, *args, **kwds):
         return message.chat.type in self.chat_type
+
+
+class ChatTypeFilter(Filter):
+    def __init__(self, chat_type: Optional[list[ChatType]] = None):
+        self.chat_type = chat_type or [ChatType.PRIVATE]
+
+    async def __call__(self, message: Message, *args, **kwds):
+        return message.chat.type in self.chat_type
