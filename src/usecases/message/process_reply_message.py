c71b24599728ab7effad64618134fc15e505fb41
Z1pP <maxm_99@mail.ru>
2025-05-18 12:57:38 +0300

    refactor: update ProcessReplyMessageUseCase to use MessageReplyRepository

--- src/usecases/message/process_reply_message.py
+++ src/usecases/message/process_reply_message.py
@@ -1,13 +1,13 @@
 from dto.message_reply import CreateMessageReplyDTO
-from repositories.message_repository import MessageRepository
+from repositories.message_reply_repository import MessageReplyRepository
 
 
 class ProcessReplyMessageUseCase:
-    def __init__(self, message_repository: MessageRepository):
-        self.message_repository = message_repository
+    def __init__(self, msg_reply_repository: MessageReplyRepository):
+        self._msg_reply_repository = msg_reply_repository
 
     async def execute(self, reply_message_dto: CreateMessageReplyDTO):
         """
         Сохраняет связь между reply-сообщением и оригинальным сообщением.
         """
-        await self.message_repository.create_reply_message(dto=reply_message_dto)
+        await self._msg_reply_repository.create_reply_message(dto=reply_message_dto)
