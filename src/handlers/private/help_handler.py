b3d549c10ea5628863df3ce93e736182b8cde057
Z1pP <maxm_99@mail.ru>
2025-05-18 23:21:32 +0300

    feat: remove unused TOLERANCE import from help_handler

--- src/handlers/private/help_handler.py
+++ src/handlers/private/help_handler.py
@@ -3,7 +3,7 @@ from aiogram.filters import Command
 from aiogram.types import Message
 
 from constants import CommandList, Dialog
-from constants.work_time import TOLERANCE, WORK_END, WORK_START
+from constants.work_time import WORK_END, WORK_START
 from utils.send_message import send_html_message
 
 router = Router(name=__name__)
@@ -11,6 +11,9 @@ router = Router(name=__name__)
 
 @router.message(Command(CommandList.HELP.name.lower()))
 async def help_handler(message: Message) -> None:
+    """
+    Выводит инструкцию по использованию бота
+    """
     help_text = Dialog.HELP_TEXT.format(
         WORK_START=WORK_START.strftime("%H.%M"),
         WORK_END=WORK_END.strftime("%H.%M"),
