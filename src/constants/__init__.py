c2dc730147f11a76123898130731cdfb1c01eb6a
Z1pP <maxm_99@mail.ru>
2025-05-18 13:07:01 +0300

    refactor: comment out unused command descriptions in CommandList

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -11,7 +11,7 @@ class CommandList(str, Enum):
     REPORT_DAILY = "Отчет о кол-ве сообщений за период. (Пример: /report_daily 21.04-25.04 @username)"
     REPORT_AVG = "Получить отчет о среднем количестве сообщений за период. Пример: /report_avg 6h"
     REPORT_RESPONSE_TIME = "Получить отчет о времени ответа на сообщения @username"
-    REPORT_INACTIVE = "Получить отчет о периодах неактивности модератора @username"
-    EXPORT_START = (
-        "Экспортировать данные в выбранном формате. Пример: /export_start csv"
-    )
+    # REPORT_INACTIVE = "Получить отчет о периодах неактивности модератора @username"
+    # EXPORT_START = (
+    #     "Экспортировать данные в выбранном формате. Пример: /export_start csv"
+    # )
