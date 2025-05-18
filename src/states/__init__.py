9bf247217c02ba21e1c43b5451cc2952029ebaf3
Z1pP <maxm_99@mail.ru>
2025-05-18 13:49:31 +0300

    refactor: remove unused state classes for moderator management

--- src/states/__init__.py
+++
@@ -1,9 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-
-class AddModeratorState(StatesGroup):
-    waiting_for_username = State()
-
-
-class RemoveModeratorState(StatesGroup):
-    waiting_for_username = State()
