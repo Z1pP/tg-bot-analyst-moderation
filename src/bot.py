7b2dcc7a9401a6899e9f3a3342ead558049a5007
Z1pP <maxm_99@mail.ru>
2025-05-21 21:10:13 +0300

    feat: initialize MemoryStorage for Dispatcher in init_bot function

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,7 @@
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
 
 from callbacks import register_callback_routers
 from commands import start_commands
@@ -15,9 +16,13 @@ async def init_bot() -> tuple[Bot, Dispatcher]:
         token=settings.BOT_TOKEN,
         default=DefaultBotProperties(parse_mode=ParseMode.HTML),
     )
+
+    # Создаем хранилище
+    storage = MemoryStorage()
+
     await start_commands.set_bot_commands(bot=bot)
 
-    dp = Dispatcher()
+    dp = Dispatcher(storage=storage)
     registry_routers(dispatcher=dp)
     registry_middlewares(dispatcher=dp)
     register_callback_routers(dispatcher=dp)
