2be06cf696ded93a72e930e324714ff1da3c4181
Z1pP <maxm_99@mail.ru>
2025-05-21 21:16:24 +0300

    feat: update start handler to include moderators list in welcome message

--- src/handlers/private/start_handler.py
+++ src/handlers/private/start_handler.py
@@ -3,7 +3,8 @@ from aiogram.filters import Command
 from aiogram.types import Message
 
 from constants import CommandList, Dialog
-from utils.send_message import send_html_message
+from keyboards.reply.menu import get_moderators_list_kb
+from utils.send_message import send_html_message_with_kb
 
 router = Router(name=__name__)
 
@@ -16,4 +17,8 @@ async def start_handler(message: Message) -> None:
     username = message.from_user.full_name
     welcome_text = Dialog.START_TEXT.format(username=username)
 
-    await send_html_message(message=message, text=welcome_text)
+    await send_html_message_with_kb(
+        message=message,
+        text=welcome_text,
+        reply_markup=get_moderators_list_kb(),
+    )
