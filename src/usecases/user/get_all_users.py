e68ac4197e97e0f7034b040db2b1af6269b63413
Z1pP <maxm_99@mail.ru>
2025-05-23 00:24:16 +0300

    refactor: remove admin_username exclusion from get_all_users execution

--- src/usecases/user/get_all_users.py
+++ src/usecases/user/get_all_users.py
@@ -6,11 +6,7 @@ class GetAllUsersUseCase:
     def __init__(self, user_repository: UserRepository):
         self._user_repository = user_repository
 
-    async def execute(self, admin_username: str) -> list[UserDTO]:
-        users = await self._user_repository.get_all_users(
-            exclude=[
-                admin_username,
-            ]
-        )
+    async def execute(self) -> list[UserDTO]:
+        users = await self._user_repository.get_all_users()
 
         return [UserDTO.from_entity(user) for user in users]
