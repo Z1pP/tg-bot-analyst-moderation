83927b668dcf02802bb919b3c32054c461b99f1a
Z1pP <maxm_99@mail.ru>
2025-05-24 17:55:50 +0300

    refactor: enhance logging setup and improve error messages in main.py

--- src/main.py
+++ src/main.py
@@ -1,23 +1,35 @@
 import asyncio
 import logging
+import os
 import sys
+from pathlib import Path
 
 from bot import init_bot
+from utils.logger_config import setup_logger
 
-logging.basicConfig(level=logging.INFO)
+# Создание папки для хранения логов
+BASE_DIR = Path(__file__).resolve().parent.parent
+LOGS_FOLDER = BASE_DIR / "logs"
+os.makedirs(LOGS_FOLDER, exist_ok=True)
+
+setup_logger(log_file=LOGS_FOLDER / "bot.log", log_level=logging.DEBUG)
 
 logger = logging.getLogger(__name__)
 
 
 async def main():
     try:
+        logger.info("Старт бота...")
         bot, dp = await init_bot()
+        logger.info("Бот запущен успешно.")
         await dp.start_polling(bot)
     except Exception as e:
-        logger.error(f"An error occurred: {e}")
+        logger.error("Непредвиденная ошибка: %s", str(e), exc_info=True)
         sys.exit(1)
     finally:
+        logger.info("Закрывает сессию...")
         await bot.session.close()
+        logger.info("Сессия закрыта успешно.")
 
 
 if __name__ == "__main__":
