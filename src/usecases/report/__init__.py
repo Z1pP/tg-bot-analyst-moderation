1884cc9fa4d489566a6f7756f0a1dbe30427e069
Z1pP <maxm_99@mail.ru>
2025-05-27 16:14:49 +0300

    feat: add GetAllModeratorsReportUseCase to module exports

--- src/usecases/report/__init__.py
+++ src/usecases/report/__init__.py
@@ -1,9 +1,11 @@
 from .get_avg_message_count_usecase import GetAvgMessageCountUseCase
 from .get_daily_report_usecase import GetDailyReportUseCase
+from .get_full_report import GetAllModeratorsReportUseCase
 from .get_response_time_report_usecase import GetResponseTimeReportUseCase
 
 __all__ = [
     "GetDailyReportUseCase",
     "GetAvgMessageCountUseCase",
     "GetResponseTimeReportUseCase",
+    "GetAllModeratorsReportUseCase",
 ]
