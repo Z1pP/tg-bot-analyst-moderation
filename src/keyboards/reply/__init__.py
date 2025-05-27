f5a6ffc92d825549138c2948d42bef6da72ca695
Z1pP <maxm_99@mail.ru>
2025-05-27 16:15:01 +0300

    feat: include get_time_period_for_full_report in module exports

--- src/keyboards/reply/__init__.py
+++ src/keyboards/reply/__init__.py
@@ -1,7 +1,8 @@
 from .menu import get_moderators_list_kb
-from .time_period import get_time_period_kb
+from .time_period import get_time_period_for_full_report, get_time_period_kb
 
 __all__ = [
     "get_time_period_kb",
     "get_moderators_list_kb",
+    "get_time_period_for_full_report",
 ]
