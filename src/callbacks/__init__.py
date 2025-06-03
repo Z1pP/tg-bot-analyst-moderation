a77f220b2a1c5627f8bddb9792745bb6dfbe7afc
Z1pP <maxm_99@mail.ru>
2025-06-03 22:53:36 +0300

    feat: register all users callback router in the dispatcher

--- src/callbacks/__init__.py
+++ src/callbacks/__init__.py
@@ -1,7 +1,9 @@
 from aiogram import Dispatcher
 
+from .all_users_callback import router as all_users_callback_router
 from .user_callback import router as user_callback_router
 
 
 def register_callback_routers(dispatcher: Dispatcher):
     dispatcher.include_router(user_callback_router)
+    dispatcher.include_router(all_users_callback_router)
