79a18143138ecd78f957d0ff83c348a12f208187
Z1pP <maxm_99@mail.ru>
2025-06-07 15:09:03 +0300

    feat: add help handler for bot usage instructions

---
+++ src/handlers/private/common/help_handler.py
@@ -0,0 +1,23 @@
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import Message
+
+from constants import CommandList, Dialog
+from constants.work_time import WORK_END, WORK_START
+from utils.send_message import send_html_message_with_kb
+
+router = Router(name=__name__)
+
+
+@router.message(Command(CommandList.HELP.name.lower()))
+async def help_handler(message: Message) -> None:
+    """
+    Выводит инструкцию по использованию бота
+    """
+    help_text = Dialog.HELP_TEXT.format(
+        WORK_START=WORK_START.strftime("%H.%M"),
+        WORK_END=WORK_END.strftime("%H.%M"),
+        TOLERANCE="10",
+    )
+
+    await send_html_message_with_kb(message=message, text=help_text)
