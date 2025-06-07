15ae29ca26c341eaa1edc10a048090983d1b00cd
Z1pP <maxm_99@mail.ru>
2025-06-07 15:09:07 +0300

    feat: implement start handler for welcome message

---
+++ src/handlers/private/common/start_handler.py
@@ -0,0 +1,24 @@
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import Message
+
+from constants import CommandList, Dialog
+from keyboards.reply.menu import get_admin_menu_kb
+from utils.send_message import send_html_message_with_kb
+
+router = Router(name=__name__)
+
+
+@router.message(Command(CommandList.START.name.lower()))
+async def start_handler(message: Message) -> None:
+    """
+    Выводит приветственное сообщение
+    """
+    username = message.from_user.full_name
+    welcome_text = Dialog.START_TEXT.format(username=username)
+
+    await send_html_message_with_kb(
+        message=message,
+        text=welcome_text,
+        reply_markup=get_admin_menu_kb(),
+    )
