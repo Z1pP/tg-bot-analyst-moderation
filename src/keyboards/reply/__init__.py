109aedcbc33f52ffa24c265bc280720e2333268c
Z1pP <maxm_99@mail.ru>
2025-06-07 14:57:36 +0300

    fix: update import to use admin menu keyboard instead of moderators list

--- src/keyboards/reply/__init__.py
+++ src/keyboards/reply/__init__.py
@@ -1,8 +1,8 @@
-from .menu import get_moderators_list_kb
+from .menu import get_admin_menu_kb
 from .time_period import get_time_period_for_full_report, get_time_period_kb
 
 __all__ = [
     "get_time_period_kb",
-    "get_moderators_list_kb",
+    "get_admin_menu_kb",
     "get_time_period_for_full_report",
 ]
