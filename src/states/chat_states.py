9bfac6d0fe9848d0977d917ecfa5137fe6cba22a
Z1pP <maxm_99@mail.ru>
2025-06-07 14:58:52 +0300

    feat: add ChatStateManager class for managing chat states

---
+++ src/states/chat_states.py
@@ -0,0 +1,7 @@
+from aiogram.fsm.state import State, StatesGroup
+
+
+class ChatStateManager(StatesGroup):
+    listing_chats = State()
+    selecting_chat = State()
+    selecting_all_chats = State()
