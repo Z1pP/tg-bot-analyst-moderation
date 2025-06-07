5bd0db49b337ff89924bfbc0ecc9840d7ca71d8d
Z1pP <maxm_99@mail.ru>
2025-06-07 14:57:42 +0300

    feat: add chat actions keyboard and utility functions

---
+++ src/keyboards/reply/chat_actions.py
@@ -0,0 +1,39 @@
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+
+from constants import KbCommands
+
+
+def chat_actions_kb(chat_title: str):
+    chat_title = shorten(chat_title)
+    buttons = [
+        [
+            KeyboardButton(text=KbCommands.SELECTED_CHAT.format(chat_title=chat_title)),
+        ],
+        [
+            KeyboardButton(text=KbCommands.REPORT_RESPONSE_TIME),
+        ],
+        [
+            KeyboardButton(text=KbCommands.MENU),
+        ],
+    ]
+
+    return ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True)
+
+
+def all_chats_actions_kb():
+    buttons = [
+        [
+            KeyboardButton(text=KbCommands.FULL_REPORT),
+        ],
+        [
+            KeyboardButton(text=KbCommands.MENU),
+        ],
+    ]
+
+    return ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True)
+
+
+def shorten(text: str, max_len: int = 25, suffix: str = "...") -> str:
+    if len(text) <= max_len:
+        return text
+    return text[:max_len].rstrip() + suffix
