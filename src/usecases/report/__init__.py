fe42ddbc4f7a1d23e3a9a1b56f7ade332d4ffc91
Z1pP <maxm_99@mail.ru>
2025-06-08 14:34:06 +0300

    feat: add GetReportOnSpecificChat to public API

--- src/usecases/report/__init__.py
+++ src/usecases/report/__init__.py
@@ -1,7 +1,9 @@
+from .get_chat_report import GetReportOnSpecificChat
 from .get_full_report import GetAllModeratorsReportUseCase
 from .get_response_time_report_usecase import GetResponseTimeReportUseCase
 
 __all__ = [
     "GetResponseTimeReportUseCase",
     "GetAllModeratorsReportUseCase",
+    "GetReportOnSpecificChat",
 ]
