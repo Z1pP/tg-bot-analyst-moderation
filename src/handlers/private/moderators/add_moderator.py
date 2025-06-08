cb38023cacdf02c56817a4a9d9fab4525c9b5af8
Z1pP <maxm_99@mail.ru>
2025-06-08 12:37:37 +0300

    feat: implement add moderator functionality with input validation

---
+++ src/handlers/private/moderators/add_moderator.py
@@ -0,0 +1,85 @@
+from aiogram import F, Router
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message
+
+from constants import Dialog, KbCommands
+from container import container
+from keyboards.reply import get_admin_menu_kb, get_back_kb
+from states.user_states import UsernameManagement
+from usecases.user import GetOrCreateUserIfNotExistUserCase
+from utils.exception_handler import handle_exception
+from utils.send_message import send_html_message_with_kb
+from utils.username_validator import validate_username
+
+router = Router(name=__name__)
+
+
+@router.message(F.text == KbCommands.ADD_MODERATOR)
+async def add_moderator_handler(message: Message, state: FSMContext) -> None:
+    """
+    Хендлер для команды добавления модератора.
+    """
+    await state.set_state(UsernameManagement.imput_username)
+
+    await send_html_message_with_kb(
+        message=message,
+        text=Dialog.INPUT_MODERATOR_USERNAME,
+        reply_markup=get_back_kb(),
+    )
+
+
+@router.message(UsernameManagement.imput_username)
+async def process_adding_moderator(message: Message, state: FSMContext) -> None:
+    """
+    Обработчик для добавления нового модератора.
+    """
+
+    if message.text == KbCommands.BACK:
+        await back_to_menu_handler(message, state)
+        return
+
+    if message.forward_from:
+        username = message.forward_from.username
+
+    username = validate_username(message.text)
+
+    if not username:
+        await send_html_message_with_kb(
+            message=message,
+            text="Неверный формат username или @username",
+        )
+        return
+
+    try:
+        use_case: GetOrCreateUserIfNotExistUserCase = container.resolve(
+            GetOrCreateUserIfNotExistUserCase
+        )
+        user = await use_case.execute(username=username)
+
+        if user.is_existed:
+            await send_html_message_with_kb(
+                message=message,
+                text=f"Пользователь <b>{username}</b> уже является модератором",
+            )
+
+        await send_html_message_with_kb(
+            message=message,
+            text=f"Пользователь <b>{username}</b> добавлен в список модераторов",
+        )
+    except Exception as e:
+        await handle_exception(message, e, "process_adding_moderator")
+    finally:
+        await state.clear()
+
+
+async def back_to_menu_handler(message: Message, state: FSMContext) -> None:
+    """
+    Обработчик для возврата в главное меню.
+    """
+    await state.clear()
+
+    await send_html_message_with_kb(
+        message=message,
+        text="Нет так нет.",
+        reply_markup=get_admin_menu_kb(),
+    )
