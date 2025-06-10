397e6d012bb08879e2c54569a3b86178223d22fa
Z1pP <maxm_99@mail.ru>
2025-06-10 22:33:25 +0300

    feat: add AdminChatAccess model for managing admin access to chats

---
+++ src/models/admin_chat_access.py
@@ -0,0 +1,36 @@
+from sqlalchemy import ForeignKey, Integer
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .base import BaseModel
+
+
+class AdminChatAccess(BaseModel):
+    """Модель для хранения доступа администраторов к чатам"""
+
+    __tablename__ = "admin_chat_access"
+
+    admin_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey(
+            "users.id",
+            ondelete="CASCADE",
+        ),
+        nullable=False,
+    )
+
+    chat_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey(
+            "chat_sessions.id",
+            ondelete="CASCADE",
+        ),
+        nullable=False,
+    )
+    # Флаг, указывающий, является ли чат источником статистики
+    is_source: Mapped[bool] = mapped_column(default=True)
+    # Флаг, указывающий, является ли чат получателем отчетов
+    is_target: Mapped[bool] = mapped_column(default=False)
+
+    # Relationships
+    admin = relationship("User", back_populates="chat_access")
+    chat = relationship("ChatSession", back_populates="admin_access")
