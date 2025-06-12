8fb88baf66b6d12ee7eeab267fcbe1e6136dd6af
Z1pP <maxm_99@mail.ru>
2025-06-12 21:57:14 +0300

    feat: update import paths and __all__ exports for report use cases

--- src/usecases/report/__init__.py
+++ src/usecases/report/__init__.py
@@ -1,9 +1,9 @@
-from .get_chat_report import GetReportOnSpecificChat
-from .get_full_report import GetAllModeratorsReportUseCase
-from .get_response_time_report_usecase import GetResponseTimeReportUseCase
+from .chat.get_chat_report import GetReportOnSpecificChatUseCase
+from .moderator.get_full_report import GetAllModeratorsReportUseCase
+from .moderator.get_specific_moderator_report import GetReportOnSpecificModeratorUseCase
 
 __all__ = [
-    "GetResponseTimeReportUseCase",
+    "GetReportOnSpecificModeratorUseCase",
     "GetAllModeratorsReportUseCase",
-    "GetReportOnSpecificChat",
+    "GetReportOnSpecificChatUseCase",
 ]
