da8af3d5815aa4a69c6dae8fac9af0a64c4b9673
Z1pP <maxm_99@mail.ru>
2025-06-14 15:47:54 +0300

    refactor: rename GetAllChatsUseCase to GetAllTargetChatsUseCase and update chat retrieval logic

--- src/usecases/chat/get_all_chats.py
+++ src/usecases/chat/get_all_chats.py
@@ -1,8 +1,9 @@
+from exceptions.user import UserNotFoundException
 from repositories import ChatTrackingRepository
 from services.user import UserService
 
 
-class GetAllChatsUseCase:
+class GetAllTargetChatsUseCase:
     def __init__(
         self,
         chat_tracking_repository: ChatTrackingRepository,
@@ -15,8 +16,10 @@ class GetAllChatsUseCase:
         user = await self._user_service.get_user(username)
 
         if not user:
-            return []
+            raise UserNotFoundException()
 
-        chats = await self._chat_tracking_repository.get_all_admin_chats(user.id)
+        chats = await self._chat_tracking_repository.get_admin_target_chats(
+            admin_id=user.id
+        )
 
         return chats
