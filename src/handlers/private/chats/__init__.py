0603fc77c27012eeaa205ce2071dcbcb17025ba5
Z1pP <maxm_99@mail.ru>
2025-06-14 15:38:55 +0300

    feat: include tracked chats router in moderators router

--- src/handlers/private/chats/__init__.py
+++ src/handlers/private/chats/__init__.py
@@ -1,6 +1,8 @@
 from aiogram import Router
 
 from .list_handler import router as moderators_list_router
+from .list_of_tracked_handler import router as tracked_chats_list_router
 
 router = Router(name="moderators_router")
 router.include_router(moderators_list_router)
+router.include_router(tracked_chats_list_router)
