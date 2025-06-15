7d2a2e6d89e2e057f0f56babb84b710a73a402b8
Z1pP <maxm_99@mail.ru>
2025-06-15 13:05:13 +0300

    fix: update import from GetAllTargetChatsUseCase to GetAllSourceChatsUseCase

--- src/usecases/chat/__init__.py
+++ src/usecases/chat/__init__.py
@@ -1,9 +1,9 @@
-from .get_all_chats import GetAllTargetChatsUseCase
+from .get_all_chats import GetAllSourceChatsUseCase
 from .get_or_create_chat import GetOrCreateChatUseCase
 from .get_tracked_chats import GetTrackedChatsUseCase
 
 __all__ = [
     "GetOrCreateChatUseCase",
-    "GetAllTargetChatsUseCase",
+    "GetAllSourceChatsUseCase",
     "GetTrackedChatsUseCase",
 ]
