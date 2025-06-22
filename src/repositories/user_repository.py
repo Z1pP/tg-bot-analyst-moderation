e5553ff6dc437066645a3b461650f5f6ba10ac31
Z1pP <maxm_99@mail.ru>
2025-06-22 12:33:01 +0300

    feat: add method to retrieve all admins from the database

--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -54,6 +54,25 @@ class UserRepository:
                 logger.error("Ошибка при получении списка модераторов: %s", e)
                 return []
 
+    async def get_all_admins(self) -> List[User]:
+        """Получаем всех администраторов из БД"""
+        async with async_session() as session:
+            try:
+                result = await session.execute(
+                    select(User)
+                    .where(
+                        User.role == UserRole.ADMIN,
+                    )
+                    .order_by(User.username),
+                )
+
+                users = result.scalars().all()
+                logger.info("Получено %d администраторов", len(users))
+                return users
+            except Exception as e:
+                logger.error("Ошибка при получении списка администраторов: %s", e)
+                return []
+
     async def get_user_by_username(self, username: str) -> Optional[User]:
         """Получает пользователя по имени пользователя."""
         async with async_session() as session:
