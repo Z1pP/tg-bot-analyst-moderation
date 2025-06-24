3ca6295b3246005575737cd9b7c72470550fbc14
Z1pP <maxm_99@mail.ru>
2025-06-25 00:25:52 +0300

    feat: add QuickResponseStateManager to exported states

--- src/states/__init__.py
+++ src/states/__init__.py
@@ -1,4 +1,9 @@
 from .chat_states import ChatStateManager
+from .response_state import QuickResponseStateManager
 from .user_states import UserStateManager
 
-__all__ = ["ChatStateManager", "UserStateManager"]
+__all__ = [
+    "ChatStateManager",
+    "UserStateManager",
+    "QuickResponseStateManager",
+]
