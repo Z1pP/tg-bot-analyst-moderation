0476e1cd2709ab20fb407d410826ac7f19467bac
Z1pP <maxm_99@mail.ru>
2025-06-25 00:29:44 +0300

    feat: add FSMContext to templates_menu_handler for state management

--- src/handlers/private/answers_templates/templates_menu.py
+++ src/handlers/private/answers_templates/templates_menu.py
@@ -1,4 +1,5 @@
 from aiogram import F, Router
+from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
 from constants import KbCommands
@@ -8,8 +9,12 @@ from utils.send_message import send_html_message_with_kb
 router = Router(name=__name__)
 
 
-@router.message(F.text == KbCommands.TEMPLATES_MENU)
-async def templates_menu_handler(message: Message):
+@router.message(
+    F.text == KbCommands.TEMPLATES_MENU,
+)
+async def templates_menu_handler(message: Message, state: FSMContext):
+    """Обработчик меню шаблонов"""
+
     text = "Выбери действие:"
 
     await send_html_message_with_kb(
