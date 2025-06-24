6e1e7f64221786e49284b8ffa0d8ccac469dae84
Z1pP <maxm_99@mail.ru>
2025-06-24 23:14:12 +0300

    feat: implement templates menu handler for user actions

---
+++ src/handlers/private/answers_templates/templates_menu.py
@@ -0,0 +1,19 @@
+from aiogram import F, Router
+from aiogram.types import Message
+
+from constants import KbCommands
+from keyboards.reply.menu import tamplates_menu_kb
+from utils.send_message import send_html_message_with_kb
+
+router = Router(name=__name__)
+
+
+@router.message(F.text == KbCommands.TEMPLATES_MENU)
+async def templates_menu_handler(message: Message):
+    text = "Выбери действие:"
+
+    await send_html_message_with_kb(
+        message=message,
+        text=text,
+        reply_markup=tamplates_menu_kb(),
+    )
