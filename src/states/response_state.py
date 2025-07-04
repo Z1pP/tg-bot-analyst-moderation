fa94076b2c490a7a8eb93dea042e179e7f2f722d
Z1pP <maxm_99@mail.ru>
2025-07-04 15:58:42 +0300

    feat: add QuickResponseStateManager with states for template and category management

---
+++ src/states/response_state.py
@@ -0,0 +1,17 @@
+from aiogram.fsm.state import State, StatesGroup
+
+
+class QuickResponseStateManager(StatesGroup):
+    templates_menu = State()
+    select_template = State()
+    select_category = State()
+    select_template_category = State()
+
+    process_category_name = State()
+
+    process_template_category = State()
+    process_template_title = State()
+    process_template_content = State()
+
+    listing_categories = State()
+    listing_templates = State()
