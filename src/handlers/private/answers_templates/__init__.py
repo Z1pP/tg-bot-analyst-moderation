73a01b7ed326366526cd32bcd0640815a51ac395
Z1pP <maxm_99@mail.ru>
2025-07-04 15:58:09 +0300

    feat: update answer templates router to include add and list template handlers

--- src/handlers/private/answers_templates/__init__.py
+++ src/handlers/private/answers_templates/__init__.py
@@ -1,6 +1,10 @@
 from aiogram import Router
 
+from .add_template import router as add_template_router
+from .list_templates import router as list_templates_router
 from .templates_menu import router as templates_menu_router
 
-router = Router(name="templates_router")
+router = Router(name="answer_templates_router")
 router.include_router(templates_menu_router)
+router.include_router(add_template_router)
+router.include_router(list_templates_router)
