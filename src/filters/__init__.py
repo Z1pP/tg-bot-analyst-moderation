6dad8cfed1857c9aa288a8054700d198b2c465e8
Z1pP <maxm_99@mail.ru>
2025-07-04 15:56:45 +0300

    feat: add filters module with admin and group filters

---
+++ src/filters/__init__.py
@@ -0,0 +1,9 @@
+from .admin_filter import AdminOnlyFilter, AllRolesFilter
+from .group_filter import ChatTypeFilter, GroupTypeFilter
+
+__all__ = [
+    "AdminOnlyFilter",
+    "AllRolesFilter",
+    "ChatTypeFilter",
+    "GroupTypeFilter",
+]
