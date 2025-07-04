a74d19e2de520b1943d9646c1e7030ccf4ac7df4
Z1pP <maxm_99@mail.ru>
2025-07-04 15:57:40 +0300

    feat: update private router to include answers templates and categories routers

--- src/handlers/private/__init__.py
+++ src/handlers/private/__init__.py
@@ -1,6 +1,7 @@
 from aiogram import Router
 
-from .answers_templates import router as templates_router
+from .answers_categories import router as answer_categories_router
+from .answers_templates import router as answers_templates_router
 from .chats import router as chats_router
 from .common import router as common_router
 from .moderators import router as moderators_router
@@ -11,4 +12,5 @@ router.include_router(common_router)
 router.include_router(moderators_router)
 router.include_router(chats_router)
 router.include_router(reports_router)
-router.include_router(templates_router)
+router.include_router(answers_templates_router)
+router.include_router(answer_categories_router)
