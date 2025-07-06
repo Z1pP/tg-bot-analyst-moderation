82cf5ea293c9cdf09e1d899b1a884cdcdc829d0a
Z1pP <maxm_99@mail.ru>
2025-07-06 15:46:36 +0300

    add  delete_template router to init

--- src/callbacks/templates_answers/__init__.py
+++ src/callbacks/templates_answers/__init__.py
@@ -1,6 +1,8 @@
 from aiogram import Router
 
+from .delete_template import router as delete_template_router
 from .select_template import router as select_template_router
 
 router = Router(name="templates_callbacks")
 router.include_router(select_template_router)
+router.include_router(delete_template_router)
