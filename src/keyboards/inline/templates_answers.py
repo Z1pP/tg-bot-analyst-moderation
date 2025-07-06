6c368828480ce86b08f347877ffb07f89adebccf
Z1pP <maxm_99@mail.ru>
2025-07-06 16:04:07 +0300

    add conf_remove_template_kb

--- src/keyboards/inline/templates_answers.py
+++ src/keyboards/inline/templates_answers.py
@@ -3,11 +3,11 @@ from typing import List
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-from models import QuickResponse
+from models import MessageTemplate
 
 
 def templates_inline_kb(
-    templates: List[QuickResponse],
+    templates: List[MessageTemplate],
 ) -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
 
@@ -25,7 +25,29 @@ def templates_inline_kb(
             InlineKeyboardButton(
                 text=f"{index + 1}. {template.title}",
                 callback_data=f"template__{template.id}",
-            )
+            ),
+            InlineKeyboardButton(
+                text="🗑 Удалить",
+                callback_data=f"remove_template__{template.id}",
+            ),
+            width=2,
         )
 
+    return builder.as_markup()
+
+
+def conf_remove_template_kb() -> InlineKeyboardBuilder:
+    builder = InlineKeyboardBuilder()
+
+    builder.row(
+        InlineKeyboardButton(
+            text="Да",
+            callback_data="conf_remove_template__yes",
+        ),
+        InlineKeyboardButton(
+            text="Нет",
+            callback_data="conf_remove_template__no",
+        ),
+        width=2,
+    )
     return builder.as_markup()
