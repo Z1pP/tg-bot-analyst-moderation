2f43b4170facbb5cadd1966ba03dc8cf64c9d449
Z1pP <maxm_99@mail.ru>
2025-07-22 21:34:46 +0300

    Refactor select_category_for_template_callback and show_templates_by_category_callback for improved clarity and efficiency; remove unused functions and streamline chat retrieval logic.

--- src/callbacks/categories/select_category.py
+++ src/callbacks/categories/select_category.py
@@ -1,5 +1,4 @@
 import logging
-from typing import List
 
 from aiogram import F, Router
 from aiogram.fsm.context import FSMContext
@@ -8,13 +7,11 @@ from aiogram.types import CallbackQuery
 from container import container
 from keyboards.inline.chats_kb import template_scope_selector_kb
 from keyboards.inline.templates_answers import templates_inline_kb
-from models import ChatSession, MessageTemplate
-from repositories import MessageTemplateRepository
+from services.answers_templates import TemplateService
 from states import TemplateStateManager
 from usecases.chat import GetTrackedChatsUseCase
 
 router = Router(name=__name__)
-
 logger = logging.getLogger(__name__)
 
 
@@ -26,47 +23,31 @@ async def select_category_for_template_callback(
     query: CallbackQuery,
     state: FSMContext,
 ) -> None:
-    """Обрабчик выбора категории при создании нового шаблона"""
-
-    category_id = int(query.data.split("__")[1])  # Парсим Id категории
+    """Обработчик выбора категории при создании нового шаблона"""
+    category_id = int(query.data.split("__")[1])
     username = query.from_user.username
 
     logger.info(
         "Была выбрана категория id=%s пользователем - %s",
         category_id,
-        query.from_user.username,
+        username,
     )
 
-    # Сохраняем выбранную категорию в состоянии
     await state.update_data(category_id=category_id)
+    await state.set_state(TemplateStateManager.process_template_chat)
 
-    try:
-        await state.set_state(TemplateStateManager.process_template_chat)
-
-        chats = await get_chats_by_username(username=username)
-
-        text = "Выбери группу для шаблона:"
-        await query.message.answer(
-            text=text,
-            reply_markup=template_scope_selector_kb(chats=chats),
-        )
-
-    except Exception as e:
-        logger.error("Ошибка при получении чатов: %s", e)
-        await query.message.answer(str(e))
-    finally:
-        await query.answer()
-
-
-async def get_chats_by_username(username: str) -> List[ChatSession]:
     try:
         usecase: GetTrackedChatsUseCase = container.resolve(GetTrackedChatsUseCase)
         chats = await usecase.execute(username=username)
-
-        return chats
+        await query.message.answer(
+            text="Выбери группу для шаблона:",
+            reply_markup=template_scope_selector_kb(chats=chats),
+        )
     except Exception as e:
         logger.error("Ошибка при получении чатов: %s", e)
-        raise "Ошибка получения чатов!"
+        await query.message.answer(f"Ошибка: {e}")
+    finally:
+        await query.answer()
 
 
 @router.callback_query(
@@ -77,9 +58,8 @@ async def show_templates_by_category_callback(
     query: CallbackQuery,
     state: FSMContext,
 ) -> None:
-    """Обрабчик выбора категории при просмотре шаблонов"""
-
-    category_id = query.data.split("__")[1]
+    """Обработчик выбора категории при просмотре шаблонов"""
+    category_id = int(query.data.split("__")[1])
 
     logger.info(
         "Была выбрана категория id=%s пользователем - %s",
@@ -88,47 +68,22 @@ async def show_templates_by_category_callback(
     )
 
     try:
-        templates = await get_templates_by_category(category_id=int(category_id))
-        total_count = await get_templates_count_by_category(
-            category_id=int(category_id)
-        )
-
-        text = "Выберите шаблон:"
+        service: TemplateService = container.resolve(TemplateService)
+        templates = await service.get_by_category(category_id=category_id)
+        total_count = await service.get_count_by_category(category_id=category_id)
 
         await query.message.edit_text(
-            text=text,
+            text="Выберите шаблон:",
             reply_markup=templates_inline_kb(
                 templates=templates,
                 total_count=total_count,
+                category_id=category_id,  # Передаем ID категории для пагинации
             ),
         )
 
         await state.update_data(category_id=category_id)
         await state.set_state(TemplateStateManager.listing_templates)
-
         await query.answer()
     except Exception as e:
         logger.error("Ошибка при получении шаблонов: %s", e)
         await query.answer("Ошибка при получении шаблонов")
-
-
-async def get_templates_by_category(category_id: int) -> List[MessageTemplate]:
-    """Получает список шаблонов сообщений исходя из выбранной категории"""
-    response_repo: MessageTemplateRepository = container.resolve(
-        MessageTemplateRepository
-    )
-
-    templates = await response_repo.get_templates_by_category_paginated(
-        category_id=category_id,
-    )
-
-    return templates
-
-
-async def get_templates_count_by_category(category_id: int) -> int:
-    """Получает общее количество шаблонов"""
-    template_repo: MessageTemplateRepository = container.resolve(
-        MessageTemplateRepository
-    )
-
-    return await template_repo.get_templates_count_by_category(category_id)
