8d7e055abde4b2a71a18df6c35072da1a679f437
Z1pP <maxm_99@mail.ru>
2025-07-22 23:08:12 +0300

    Refactor template router import in callback initialization

--- src/callbacks/__init__.py
+++ src/callbacks/__init__.py
@@ -3,11 +3,11 @@ from aiogram import Dispatcher
 from .categories import router as categories_router
 from .chats import router as chats_router
 from .moderators import router as moderators_router
-from .templates_answers import router as templates_answers_router  # noqa: F401
+from .templates import router as templates_router  # noqa: F401
 
 
 def register_callback_routers(dispatcher: Dispatcher):
     dispatcher.include_router(moderators_router)
     dispatcher.include_router(chats_router)
     dispatcher.include_router(categories_router)
-    dispatcher.include_router(templates_answers_router)
+    dispatcher.include_router(templates_router)
