27fa82bccef9e1734f9e875bb3d5bc51db155520
Z1pP <maxm_99@mail.ru>
2025-07-22 22:33:19 +0300

    Refactor template retrieval to use TemplateService for improved data handling

--- src/handlers/private/answers_templates/list_templates.py
+++ src/handlers/private/answers_templates/list_templates.py
@@ -6,7 +6,7 @@ from constants import KbCommands
 from container import container
 from keyboards.inline.templates_answers import templates_inline_kb
 from keyboards.reply.menu import tamplates_menu_kb
-from repositories import MessageTemplateRepository
+from services.answers_templates import TemplateService
 from states import TemplateStateManager
 from utils.exception_handler import handle_exception
 from utils.send_message import send_html_message_with_kb
@@ -24,13 +24,9 @@ async def templates_list_handler(message: Message, state: FSMContext) -> None:
         await state.clear()  # Очистка состояния перед выводом списка чатов
         await state.set_state(TemplateStateManager.listing_templates)
 
-        # Получение всех шаблонов из БД
-        # TODO: Декомпозировать по сервисам и юзкейсам
-        template_repo: MessageTemplateRepository = container.resolve(
-            MessageTemplateRepository
-        )
-        templates = await template_repo.get_templates_paginated()
-        total_count = await template_repo.get_templates_count()
+        template_service: TemplateService = container.resolve(TemplateService)
+
+        templates, total_count = await template_service.get_templates_with_count()
 
         if not templates:
             await send_html_message_with_kb(
@@ -42,7 +38,7 @@ async def templates_list_handler(message: Message, state: FSMContext) -> None:
 
         await send_html_message_with_kb(
             message=message,
-            text=f"Всего {total_count} шаблонов",
+            text=f"Найдено {total_count} шаблонов:",
             reply_markup=templates_inline_kb(
                 templates=templates,
                 total_count=total_count,
