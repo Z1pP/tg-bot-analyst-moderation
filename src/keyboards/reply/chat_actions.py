f9ea0afe4b48db57ca5ea095edf4ba3165183ce7
Z1pP <maxm_99@mail.ru>
2025-07-24 12:41:45 +0300

    remove unuse buttons

--- src/keyboards/reply/chat_actions.py
+++ src/keyboards/reply/chat_actions.py
@@ -3,12 +3,8 @@ from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 from constants import KbCommands
 
 
-def chat_actions_kb(chat_title: str):
-    chat_title = shorten(chat_title)
+def chat_actions_kb():
     buttons = [
-        [
-            KeyboardButton(text=KbCommands.SELECTED_CHAT.format(chat_title=chat_title)),
-        ],
         [
             KeyboardButton(text=KbCommands.GET_REPORT),
         ],
@@ -40,9 +36,3 @@ def all_chats_actions_kb():
     ]
 
     return ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True)
-
-
-def shorten(text: str, max_len: int = 25, suffix: str = "...") -> str:
-    if len(text) <= max_len:
-        return text
-    return text[:max_len].rstrip() + suffix
