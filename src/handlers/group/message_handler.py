1f648563873a59f086b937d655096b2cd92a74cf
Z1pP <maxm_99@mail.ru>
2025-08-02 15:23:05 +0300

    update

--- src/handlers/group/message_handler.py
+++ src/handlers/group/message_handler.py
@@ -3,16 +3,19 @@ import logging
 from aiogram import Router
 from aiogram.types import Message
 
+from constants.enums import UserRole
 from container import container
 from dto.message import CreateMessageDTO
 from dto.message_reply import CreateMessageReplyDTO
-from filters.group_filter import GroupTypeFilter
 from models import ChatSession, User
 from models.message import MessageType
 from services.chat import ChatService
 from services.time_service import TimeZoneService
 from services.user import UserService
-from usecases.message import ProcessMessageUseCase, ProcessReplyMessageUseCase
+from usecases.message import (
+    SaveModeratorMessageUseCase,
+    SaveModeratorReplyMessageUseCase,
+)
 
 router = Router(name=__name__)
 logger = logging.getLogger(__name__)
@@ -25,10 +28,10 @@ def _get_message_type(message: Message) -> MessageType:
         return MessageType.MESSAGE
 
 
-@router.message(GroupTypeFilter())
+@router.message()
 async def group_message_handler(message: Message):
     """
-    Обрабатывет сообщения которые приходит только от модераторов чата
+    Обрабатывет сообщения которые приходят от всех пользователей
     """
     # Сообщения от ботов не сохраняются
     if message.from_user.is_bot:
@@ -36,26 +39,61 @@ async def group_message_handler(message: Message):
 
     sender, chat = await _get_sender_and_chat(message=message)
 
-    # Обрабатываем сообщение в зависимости от типа
+    # Получаем тип сообщения из message
     msg_type = _get_message_type(message=message)
+
+    if sender.role == UserRole.USER:
+        await process_user_message(
+            message=message,
+            sender=sender,
+            chat=chat,
+        )
+        return
+
     if msg_type == MessageType.REPLY:
-        await process_moderator_reply(
+        await process_reply_message(
             message=message,
             sender=sender,
             chat=chat,
         )
     else:
-        await process_moderator_message(
+        await process_reqular_message(
             message=message,
             sender=sender,
             chat=chat,
         )
 
 
-async def process_moderator_reply(message: Message, sender, chat):
+async def process_user_message(
+    message: Message,
+    sender: User,
+    chat: ChatSession,
+) -> None:
     """
-    Обрабатывает reply-сообщения модераторов.
-    Сохраняет reply как обычное сообщение, а затем создаёт связь в MessageReply.
+    Обрабатывает обычное сообщение от пользователей
+    """
+    message_date = TimeZoneService.convert_to_local_time(
+        dt=message.date,
+    )
+
+    msg_dto = CreateMessageDTO(
+        chat_id=chat.id,
+        user_id=sender.id,
+        message_id=str(message.message_id),
+        message_type=MessageType.MESSAGE.value,
+        content_type=message.content_type.value,
+        text=message.text,
+        created_at=message_date,
+    )
+
+
+async def process_reply_message(
+    message: Message,
+    sender: User,
+    chat: ChatSession,
+) -> None:
+    """
+    Обрабатывает reply-сообщения от модераторов и админов
     """
     # Преобразуем время сообщения в локальное время
     message_date = TimeZoneService.convert_to_local_time(
@@ -78,7 +116,9 @@ async def process_moderator_reply(message: Message, sender, chat):
 
     try:
         # Сохраняем reply как обычное сообщение
-        message_usecase = container.resolve(ProcessMessageUseCase)
+        message_usecase: SaveModeratorMessageUseCase = container.resolve(
+            SaveModeratorMessageUseCase
+        )
         saved_message = await message_usecase.execute(message_dto=msg_dto)
 
         # Создаем ссылку на оригинальное сообщение
@@ -98,17 +138,21 @@ async def process_moderator_reply(message: Message, sender, chat):
         )
 
         # Сохраняем связь в MessageReply
-        reply_usecase: ProcessReplyMessageUseCase = container.resolve(
-            ProcessReplyMessageUseCase
+        reply_usecase: SaveModeratorReplyMessageUseCase = container.resolve(
+            SaveModeratorReplyMessageUseCase
         )
         await reply_usecase.execute(reply_message_dto=reply_dto)
     except Exception as e:
         logger.error("Error saving reply message: %s", str(e))
 
 
-async def process_moderator_message(message: Message, sender, chat):
+async def process_reqular_message(
+    message: Message,
+    sender: User,
+    chat: ChatSession,
+) -> None:
     """
-    Обрабатывает сообщения которые не являются reply
+    Обрабатывает обычные сообщения от модераторов и админов
     """
     # Преобразуем время сообщения в локальное время
     message_date = TimeZoneService.convert_to_local_time(
@@ -126,7 +170,9 @@ async def process_moderator_message(message: Message, sender, chat):
     )
 
     try:
-        usecase = container.resolve(ProcessMessageUseCase)
+        usecase: SaveModeratorMessageUseCase = container.resolve(
+            SaveModeratorMessageUseCase
+        )
         await usecase.execute(message_dto=msg_dto)
     except Exception as e:
         logger.error("Error saving message: %s", str(e))
@@ -137,21 +183,19 @@ async def _get_sender_and_chat(message: Message) -> tuple[User, ChatSession]:
     Получает пользователя и чат из сообщения.
     """
     # Получаем сервисы
-    user_service = container.resolve(UserService)
-    chat_service = container.resolve(ChatService)
+    user_service: UserService = container.resolve(UserService)
+    chat_service: ChatService = container.resolve(ChatService)
 
     # Получаем пользователя и чат
     username = message.from_user.username
+    tg_id = str(message.from_user.id)
     chat_id = str(message.chat.id)
 
     if not username:
         logger.warning("Пользователь без username: %s", message.from_user.id)
         return
 
-    sender = await user_service.get_user(username)
-    if not sender:
-        logger.warning("Пользователь не найден в базе данных: %s", username)
-        return
+    sender = await user_service.get_or_create(username=username, tg_id=tg_id)
 
     chat = await chat_service.get_or_create_chat(
         chat_id=chat_id, title=message.chat.title or "Без названия"
