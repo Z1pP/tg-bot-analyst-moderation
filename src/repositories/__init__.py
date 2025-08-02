9eb32ad680d2fcb151bbacc0f335ed5eec6d2899
Z1pP <maxm_99@mail.ru>
2025-08-02 15:12:27 +0300

    added MessageReactionRepository in init

--- src/repositories/__init__.py
+++ src/repositories/__init__.py
@@ -5,6 +5,7 @@ from .chat_tracking_repository import ChatTrackingRepository
 from .media_repository import TemplateMediaRepository
 from .message_reply_repository import MessageReplyRepository
 from .message_repository import MessageRepository
+from .reaction_repository import MessageReactionRepository
 from .template_repository import MessageTemplateRepository
 from .user_repository import UserRepository
 
@@ -18,4 +19,5 @@ __all__ = [
     "TemplateCategoryRepository",
     "MessageTemplateRepository",
     "TemplateMediaRepository",
+    "MessageReactionRepository",
 ]
