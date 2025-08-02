3faf62c01f64ca579efda8fe086539080746b22b
Z1pP <maxm_99@mail.ru>
2025-08-02 15:09:33 +0300

    added router to init file

--- src/handlers/group/__init__.py
+++ src/handlers/group/__init__.py
@@ -3,8 +3,10 @@ from aiogram import Router
 from .chat_tracking_handler import router as chat_tracking_router
 from .message_handler import router as message_router
 from .query_handler import router as inline_query_router
+from .reactions import router as reactions_router
 
 router = Router(name="group_router")
 router.include_router(inline_query_router)
 router.include_router(chat_tracking_router)
 router.include_router(message_router)
+router.include_router(reactions_router)
