6ea57f34b554b89678aff392a2b3932495c45ebd
Z1pP <maxm_99@mail.ru>
2025-08-09 14:59:30 +0300

    added conf_remove_user_kb inline keyboard

--- src/keyboards/inline/moderators.py
+++ src/keyboards/inline/moderators.py
@@ -1,4 +1,5 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 from dto.user import UserDTO
 
@@ -36,9 +37,26 @@ def remove_inline_kb(users: list[UserDTO]):
             [
                 InlineKeyboardButton(
                     text=f"Удалить {user.username}",
-                    callback_data=f"remove_user__{user.username}",
+                    callback_data=f"remove_user__{user.id}",
                 )
             ]
         )
 
     return InlineKeyboardMarkup(inline_keyboard=keyboards)
+
+
+def conf_remove_user_kb() -> InlineKeyboardMarkup:
+    builder = InlineKeyboardBuilder()
+
+    builder.row(
+        InlineKeyboardButton(
+            text="Да",
+            callback_data="conf_remove_user__yes",
+        ),
+        InlineKeyboardButton(
+            text="Нет",
+            callback_data="conf_remove_user__no",
+        ),
+        width=2,
+    )
+    return builder.as_markup()
