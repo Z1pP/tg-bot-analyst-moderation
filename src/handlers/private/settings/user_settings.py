632ea65e91ed4b7e0edaa55b19e6f6fbfeab9ad8
Z1pP <maxm_99@mail.ru>
2025-08-09 15:51:58 +0300

    commited code

--- src/handlers/private/settings/user_settings.py
+++ src/handlers/private/settings/user_settings.py
@@ -8,7 +8,7 @@ router = Router(name=__name__)
 logger = logging.getLogger(__name__)
 
 
-@router.message(F.text == KbCommands.SETTINGS)
-async def setting_handler(message: types.Message):
-    logger.info("Вызван setting_handler")
-    await message.answer(f"Вкладка {KbCommands.SETTINGS} еще в разработке!")
+# @router.message(F.text == KbCommands.SETTINGS)
+# async def setting_handler(message: types.Message):
+#     logger.info("Вызван setting_handler")
+#     await message.answer(f"Вкладка {KbCommands.SETTINGS} еще в разработке!")
