d9986560eb856aeff676a5613bad84d529a11e54
Z1pP <maxm_99@mail.ru>
2025-08-09 15:52:14 +0300

    added  MenuStates to init file

--- src/states/__init__.py
+++ src/states/__init__.py
@@ -1,4 +1,5 @@
 from .chat_states import ChatStateManager
+from .menu_states import MenuStates
 from .template_state import TemplateStateManager
 from .user_states import UserStateManager
 
@@ -6,4 +7,5 @@ __all__ = [
     "ChatStateManager",
     "UserStateManager",
     "TemplateStateManager",
+    "MenuStates",
 ]
