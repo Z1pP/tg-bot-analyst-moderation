6b1fdb6fea355cc4a827accbaf78836724266803
Z1pP <maxm_99@mail.ru>
2025-08-09 14:59:08 +0300

    rename to SELECT_USER

--- src/handlers/private/moderators/list_handler.py
+++ src/handlers/private/moderators/list_handler.py
@@ -12,7 +12,7 @@ from utils.send_message import send_html_message_with_kb
 router = Router(name=__name__)
 
 
-@router.message(F.text == KbCommands.SELECT_MODERATOR)
+@router.message(F.text == KbCommands.SELECT_USER)
 async def moderators_list_handler(message: Message) -> None:
     """
     Обработчик команды для получения списка модераторов.
