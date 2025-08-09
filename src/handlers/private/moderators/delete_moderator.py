acf9916ca069c709e69b975afe8832df3f74d337
Z1pP <maxm_99@mail.ru>
2025-08-09 14:58:53 +0300

    the logic for removing users from tracking has been implemented

--- src/handlers/private/moderators/delete_moderator.py
+++ src/handlers/private/moderators/delete_moderator.py
@@ -4,16 +4,20 @@ from aiogram.types import CallbackQuery, Message
 
 from constants import KbCommands
 from container import container
-from keyboards.inline.moderators import remove_inline_kb
-from keyboards.reply.menu import admin_menu_kb
-from usecases.user import DeleteUserUseCase, GetAllUsersUseCase
+from keyboards.inline.moderators import conf_remove_user_kb, remove_inline_kb
+from keyboards.reply.menu import admin_menu_kb, moderator_menu_kb
+from usecases.user import (
+    DeleteUserUseCase,
+    GetAllUsersUseCase,
+    GetUserByIdUseCase,
+)
 from utils.exception_handler import handle_exception
 from utils.send_message import send_html_message_with_kb
 
 router = Router(name=__name__)
 
 
-@router.message(F.text == KbCommands.REMOVE_MODERATOR)
+@router.message(F.text == KbCommands.REMOVE_USER)
 async def remove_moderator_handler(message: Message) -> None:
     """
     Обработчик команды для получения списка модераторов.
@@ -47,24 +51,69 @@ async def remove_moderator_handler(message: Message) -> None:
 
 @router.callback_query(F.data.startswith("remove_user__"))
 async def process_removing_moderator(
-    callback: CallbackQuery, state: FSMContext
+    callback: CallbackQuery,
+    state: FSMContext,
 ) -> None:
     """
     Обработчик для удаления модератора.
     """
-    username = callback.data.split("__")[1]
-
-    use_case: DeleteUserUseCase = container.resolve(DeleteUserUseCase)
-
+    user_id = int(callback.data.split("__")[1])
     try:
-        await callback.answer()
+        use_case: GetUserByIdUseCase = container.resolve(GetUserByIdUseCase)
+        user = await use_case.execute(user_id=user_id)
 
-        await use_case.execute(username=username)
+        await state.update_data(user_id=user_id, username=user.username)
+
+        message_text = (
+            f"❗Вы уверены, что хотите удалить @{user.username} из отслеживаемых?"
+        )
+
         await send_html_message_with_kb(
             message=callback.message,
-            text=f"Пользователь <b>{username}</b> удален из списка модераторов",
+            text=message_text,
+            reply_markup=conf_remove_user_kb(),
         )
     except Exception as e:
-        await handle_exception(callback.message, e, "удалении модератора")
+        await handle_exception(callback.message, e, "process_removing_moderator")
     finally:
+        await callback.answer()
+
+
+@router.callback_query(F.data.startswith("conf_remove_user__"))
+async def confirmation_removing_user(
+    callback: CallbackQuery,
+    state: FSMContext,
+) -> None:
+    """
+    Обработчик подтверждения удаления пользователя из отслеживания
+    """
+    data = await state.get_data()
+    user_id = data.get("user_id")
+    username = data.get("username")
+
+    try:
+        answer = callback.data.split("__")[1]
+
+        if answer == "yes":
+            delete_usecase: DeleteUserUseCase = container.resolve(DeleteUserUseCase)
+            success = await delete_usecase.execute(user_id=user_id)
+
+            if success:
+                text = (
+                    f"✅ Готово! @{username} удалён из отлеживания!\n\n"
+                    "❗️Вы всегда можете вернуть пользователя "
+                    "в отслеживаемые и продолжить собирать статистику"
+                )
+            else:
+                text = f"❌ Пользователь @{username} не найден или уже удален."
+
+            await callback.message.edit_text(text=text)
+        else:
+            await callback.message.edit_text(
+                text=f"❌ Удаление @{username} отменено!",
+            )
+    except Exception as e:
+        await handle_exception(callback.message, e, "confirmation_removing_user")
+    finally:
+        await callback.answer()
         await state.clear()
