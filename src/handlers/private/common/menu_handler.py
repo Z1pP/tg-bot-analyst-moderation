d595250376f473a554d320c4415538afc8413e47
Z1pP <maxm_99@mail.ru>
2025-08-09 15:51:15 +0300

    added setting_handler and added logging states

--- src/handlers/private/common/menu_handler.py
+++ src/handlers/private/common/menu_handler.py
@@ -1,3 +1,5 @@
+import logging
+
 from aiogram import F, Router
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
@@ -5,25 +7,22 @@ from aiogram.types import Message
 
 from constants import Dialog, KbCommands
 from keyboards.reply.menu import admin_menu_kb, chat_menu_kb, user_menu_kb
+from states import MenuStates
 from utils.send_message import send_html_message_with_kb
+from utils.state_logger import log_and_set_state
 
 router = Router(name=__name__)
+logger = logging.getLogger(__name__)
 
 
 @router.message(F.text == KbCommands.MENU)
 @router.message(Command("menu"))
 @router.message(Command("start"))
 async def menu_handler(message: Message, state: FSMContext) -> None:
-    """
-    Обработчик команды /menu, /start и кнопки "Главное меню".
-    Отображает главное меню бота.
-    """
-    # Очищаем состояние FSM
-    await state.clear()
+    await log_and_set_state(message, state, MenuStates.main_menu)
 
     username = message.from_user.first_name
-
-    menu_text = Dialog.START_TEXT.format(username=username)
+    menu_text = Dialog.MENU_TEXT.format(username=username)
 
     await send_html_message_with_kb(
         message=message,
@@ -33,7 +32,8 @@ async def menu_handler(message: Message, state: FSMContext) -> None:
 
 
 @router.message(F.text == KbCommands.USERS_MENU)
-async def users_menu_handler(message: Message) -> None:
+async def users_menu_handler(message: Message, state: FSMContext) -> None:
+    await log_and_set_state(message, state, MenuStates.users_menu)
 
     await send_html_message_with_kb(
         message=message,
@@ -43,10 +43,18 @@ async def users_menu_handler(message: Message) -> None:
 
 
 @router.message(F.text == KbCommands.CHATS_MENU)
-async def chats_menu_handler(message: Message) -> None:
+async def chats_menu_handler(message: Message, state: FSMContext) -> None:
+    await log_and_set_state(message, state, MenuStates.chats_menu)
 
     await send_html_message_with_kb(
         message=message,
         text=Dialog.CHATS_MENU_TEXT,
         reply_markup=chat_menu_kb(),
     )
+
+
+@router.message(F.text == KbCommands.SETTINGS)
+async def setting_handler(message: Message, state: FSMContext) -> None:
+    await log_and_set_state(message, state, MenuStates.settings_menu)
+
+    await message.answer(f"Вкладка {KbCommands.SETTINGS} еще в разработке!")
