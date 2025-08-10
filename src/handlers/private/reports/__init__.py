8bf40748ecb1f4fc344bf7688b714dbbf3b4c10c
Z1pP <maxm_99@mail.ru>
2025-08-10 13:12:39 +0300

    rename imports

--- src/handlers/private/reports/__init__.py
+++ src/handlers/private/reports/__init__.py
@@ -1,8 +1,8 @@
 from aiogram import Router
 
-from .chat_report import router as chat_report_router
-from .full_report import router as full_moderators_report_router
-from .response_time import router as response_time_report_router
+from .all_users_report import router as full_moderators_report_router
+from .single_chat_report import router as chat_report_router
+from .single_user_report import router as response_time_report_router
 
 router = Router(name="report_router")
 router.include_router(chat_report_router)
