42f20f03236b2cacaa9fa7e51802e4f1135a0036
Z1pP <maxm_99@mail.ru>
2025-08-13 23:53:31 +0300

    Refactor chats_list_handler to improve state management and user feedback for empty chat lists

--- src/handlers/private/chats/list_handler.py
+++ src/handlers/private/chats/list_handler.py
@@ -5,33 +5,53 @@ from aiogram.types import Message
 from constants import KbCommands
 from container import container
 from keyboards.inline.chats_kb import chats_inline_kb
-from states import ChatStateManager
+from keyboards.reply import chat_menu_kb
+from states import ChatStateManager, MenuStates
 from usecases.chat import GetAllChatsUseCase
 from utils.exception_handler import handle_exception
 from utils.send_message import send_html_message_with_kb
+from utils.state_logger import log_and_set_state
 
 router = Router(name=__name__)
 
 
-@router.message(F.text == KbCommands.SELECT_CHAT)
+@router.message(
+    F.text == KbCommands.GET_STATISTICS,
+    MenuStates.chats_menu,
+)
 async def chats_list_handler(message: Message, state: FSMContext) -> None:
     """
     Обработчик команды для получения списка чатов.
     """
 
     try:
-        # Состояние вывода списка чатов
-        await state.set_state(ChatStateManager.listing_chats)
-
         username = message.from_user.username
 
         # Получение чатов из БД
         usecase: GetAllChatsUseCase = container.resolve(GetAllChatsUseCase)
         chats = await usecase.execute(username)
 
+        if not chats:
+            message_text = (
+                "❗Чтобы получать отчёты по чату, необходимо добавить чат "
+                "в отслеживаемые, а также пользователей для сбора статистики"
+            )
+            await send_html_message_with_kb(
+                message=message,
+                text=message_text,
+                reply_markup=chat_menu_kb(),
+            )
+            return
+
+        await log_and_set_state(
+            message=message,
+            state=state,
+            new_state=ChatStateManager.listing_chats,
+        )
+
         await send_html_message_with_kb(
             message=message,
-            text=f"Всего {len(chats)} чатов",
+            text=f"Найдено {len(chats)} чат(-ов):",
             reply_markup=chats_inline_kb(chats=chats),
         )
     except Exception as e:
