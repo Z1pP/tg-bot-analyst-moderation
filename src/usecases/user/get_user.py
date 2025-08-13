88253e8a010b110a1c8bf6f341b706d39769935a
Z1pP <maxm_99@mail.ru>
2025-08-13 21:15:11 +0300

    Rename GetUserByUsernameUseCase to GetUserByTgIdUseCase and update method to fetch user by Telegram ID

--- src/usecases/user/get_user.py
+++ src/usecases/user/get_user.py
@@ -4,12 +4,12 @@ from models import User
 from repositories import UserRepository
 
 
-class GetUserByUsernameUseCase:
+class GetUserByTgIdUseCase:
     def __init__(self, user_repository: UserRepository):
         self.user_repository = user_repository
 
-    async def execute(self, username: str) -> Optional[User]:
-        return await self.user_repository.get_user_by_username(username)
+    async def execute(self, tg_id: str) -> Optional[User]:
+        return await self.user_repository.get_user_by_tg_id(tg_id=tg_id)
 
 
 class GetUserByIdUseCase:
