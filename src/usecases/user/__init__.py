1aa7362d8358e102625bb20525be085afc5e13f1
Z1pP <maxm_99@mail.ru>
2025-08-13 21:14:21 +0300

    Fix import and export of GetUserByUsernameUseCase to GetUserByTgIdUseCase for consistency

--- src/usecases/user/__init__.py
+++ src/usecases/user/__init__.py
@@ -1,14 +1,14 @@
 from .create_new_user import CreateNewUserUserCase
 from .get_all_users import GetAllUsersUseCase
 from .get_or_create_user import GetOrCreateUserIfNotExistUserCase
-from .get_user import GetUserByIdUseCase, GetUserByUsernameUseCase
+from .get_user import GetUserByIdUseCase, GetUserByTgIdUseCase
 from .remove_user import DeleteUserUseCase
 
 __all__ = [
     "GetOrCreateUserIfNotExistUserCase",
     "CreateNewUserUserCase",
     "DeleteUserUseCase",
-    "GetUserByUsernameUseCase",
+    "GetUserByTgIdUseCase",
     "GetAllUsersUseCase",
     "GetUserByIdUseCase",
 ]
