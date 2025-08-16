c5c5fede48cbdea90500f94587b9824402984190
Z1pP <maxm_99@mail.ru>
2025-08-16 23:38:54 +0300

    Refactor list_of_tracking_chats_handler to improve state management and use user ID for fetching tracked chats

--- src/handlers/private/chats/list_of_tracked_handler.py
+++ src/handlers/private/chats/list_of_tracked_handler.py
@@ -7,38 +7,52 @@ from aiogram.types import Message
 from constants import KbCommands
 from container import container
 from keyboards.inline.chats_kb import tracked_chats_inline_kb
+from keyboards.reply import chat_menu_kb
+from states import MenuStates
 from states.chat_states import ChatStateManager
 from usecases.chat import GetTrackedChatsUseCase
 from utils.exception_handler import handle_exception
 from utils.send_message import send_html_message_with_kb
+from utils.state_logger import log_and_set_state
 
 logger = logging.getLogger(__name__)
 
 router = Router(name=__name__)
 
 
-@router.message(F.text == KbCommands.TRACKED_CHATS)
+@router.message(F.text == KbCommands.GET_STATISTICS, MenuStates.chats_menu)
+@router.message(F.text == KbCommands.SELECT_CHAT)
 async def list_of_tracking_chats_handler(message: Message, state: FSMContext) -> None:
     """
     Обработчик команды для получения списка трекающих чатов.
     """
     try:
-        # Устанавливаем состояние вывода списка трекающих чатов
-        await state.set_state(ChatStateManager.listing_tracking_chats)
-
-        username = message.from_user.username
+        tg_id = str(message.from_user.id)
 
         usecase: GetTrackedChatsUseCase = container.resolve(GetTrackedChatsUseCase)
-        chats = await usecase.execute(username)
+        chats = await usecase.execute(tg_id=tg_id)
 
-        text = (
-            "<b>Управление отслеживаемыми чатами</b>\n\n" "Пожалуйста, выберу группу:"
+        if not chats:
+            message_text = (
+                "❗Чтобы получать отчёты по чату, необходимо добавить чат "
+                "в отслеживаемые, а также пользователей для сбора статистики"
+            )
+            await send_html_message_with_kb(
+                message=message,
+                text=message_text,
+                reply_markup=chat_menu_kb(),
+            )
+            return
+
+        await log_and_set_state(
+            message=message,
+            state=state,
+            new_state=ChatStateManager.listing_tracking_chats,
         )
-
         # Отправляем сообщение с кнопками для выбора действия
         await send_html_message_with_kb(
             message=message,
-            text=text,
+            text=f"Найдено {len(chats)} чат(-ов):",
             reply_markup=tracked_chats_inline_kb(chats=chats),
         )
     except Exception as e:
