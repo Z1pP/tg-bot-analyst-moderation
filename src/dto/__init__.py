f428d9ace947858b91e4ccc8743942fd9f5dfdd2
Z1pP <maxm_99@mail.ru>
2025-09-02 01:50:26 +0300

    registered dto

--- src/dto/__init__.py
+++ src/dto/__init__.py
@@ -1,9 +1,12 @@
 from .chat_dto import ChatDTO
+from .daily_activity import ChatDailyStatsDTO, UserDailyActivityDTO
 from .reaction import MessageReactionDTO
 from .user import UserDTO
 
 __all__ = [
     "ChatDTO",
+    "ChatDailyStatsDTO",
     "MessageReactionDTO",
+    "UserDailyActivityDTO",
     "UserDTO",
 ]
