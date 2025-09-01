b9275b49821056d4c8ae2a5569103322fe4df659
Z1pP <maxm_99@mail.ru>
2025-09-02 01:52:21 +0300

    register router

--- src/handlers/private/chats/__init__.py
+++ src/handlers/private/chats/__init__.py
@@ -1,10 +1,12 @@
 from aiogram import Router
 
 from .add_chat import router as add_chat_router
+from .chat_daily_rating import router as chat_daily_rating_router
 from .delete_chat import router as delete_chat_router
 from .list_of_tracked_handler import router as tracked_chats_list_router
 
 router = Router(name="chats_router")
 router.include_router(add_chat_router)
+router.include_router(chat_daily_rating_router)
 router.include_router(delete_chat_router)
 router.include_router(tracked_chats_list_router)
