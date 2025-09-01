401ee2e6309815afd764a259a81e956b40fd7910
Z1pP <maxm_99@mail.ru>
2025-09-01 16:43:38 +0300

    added router to init fole

--- src/callbacks/templates/__init__.py
+++ src/callbacks/templates/__init__.py
@@ -1,6 +1,7 @@
 from aiogram import Router
 
 from .delete_template import router as delete_template_router
+from .edit_template import router as edit_template_router
 from .pagination import router as pagination_router
 from .select_template import router as select_template_router
 
@@ -8,3 +9,4 @@ router = Router(name="templates_callback")
 router.include_router(pagination_router)
 router.include_router(select_template_router)
 router.include_router(delete_template_router)
+router.include_router(edit_template_router)
--- src/handlers/private/categories/__init__.py
+++ src/handlers/private/categories/__init__.py
@@ -1,8 +1,10 @@
 from aiogram import Router
 
 from .add_category import router as add_category_router
+from .edit_category import router as edit_category_router
 from .list_categories import router as list_categories_router
 
 router = Router(name="answer_category_router")
 router.include_router(add_category_router)
 router.include_router(list_categories_router)
+router.include_router(edit_category_router)
