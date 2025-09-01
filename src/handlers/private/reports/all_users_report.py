ac9f3bb2acfe05659820129975bacc43a7e709a3
Z1pP <maxm_99@mail.ru>
2025-09-01 18:36:23 +0300

    remove List from imports

--- src/handlers/private/reports/all_users_report.py
+++ src/handlers/private/reports/all_users_report.py
@@ -1,6 +1,6 @@
 import logging
 from datetime import datetime
-from typing import List, Optional
+from typing import Optional
 
 from aiogram import F, Router
 from aiogram.fsm.context import FSMContext
