a8d2170d6597fcf614c569bad8deba9364702c76
Z1pP <maxm_99@mail.ru>
2025-09-01 16:12:51 +0300

    added pagination_router to init file

--- src/callbacks/categories/__init__.py
+++ src/callbacks/categories/__init__.py
@@ -1,8 +1,10 @@
 from aiogram import Router
 
 from .delete_category import router as delete_category_router
+from .pagination import router as pagination_router
 from .select_category import router as select_category_router
 
 router = Router(name="categories_callbacks")
 router.include_router(select_category_router)
 router.include_router(delete_category_router)
+router.include_router(pagination_router)
