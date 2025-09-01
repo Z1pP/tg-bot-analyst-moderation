3b343f97f903bf142d29458580403e5c69379a49
Z1pP <maxm_99@mail.ru>
2025-09-01 18:36:09 +0300

    added GetChatBreaksDetailReportUseCase

--- src/usecases/report/__init__.py
+++ src/usecases/report/__init__.py
@@ -1,5 +1,8 @@
+from .chat.get_chat_breaks_detail_report import GetChatBreaksDetailReportUseCase
 from .chat.get_chat_report import GetReportOnSpecificChatUseCase
-from .user.get_all_users_breaks_detail_report import GetAllUsersBreaksDetailReportUseCase
+from .user.get_all_users_breaks_detail_report import (
+    GetAllUsersBreaksDetailReportUseCase,
+)
 from .user.get_all_users_report import GetAllUsersReportUseCase
 from .user.get_breaks_details import GetBreaksDetailReportUseCase
 from .user.get_single_user_report import GetSingleUserReportUseCase
@@ -10,4 +13,5 @@ __all__ = [
     "GetReportOnSpecificChatUseCase",
     "GetBreaksDetailReportUseCase",
     "GetAllUsersBreaksDetailReportUseCase",
+    "GetChatBreaksDetailReportUseCase",
 ]
