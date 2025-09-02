358c67ee1d4cf0cfa21e1714a4c21677b83499af
Z1pP <maxm_99@mail.ru>
2025-09-02 03:21:08 +0300

    all dtos in init

--- src/dto/__init__.py
+++ src/dto/__init__.py
@@ -1,19 +1,61 @@
-from .chat_dto import ChatDTO
+from .activity import CreateActivityDTO, ResultActivityDTO
+from .category_dto import CategoryDTO, CreateCategoryDTO, UpdateCategoryDTO
+from .chat_dto import ChatDTO, UserChatsDTO
 from .daily_activity import (
     ChatDailyStatsDTO,
     PopularReactionDTO,
     UserDailyActivityDTO,
     UserReactionActivityDTO,
 )
+from .message import CreateMessageDTO, ResultMessageDTO
+from .message_reply import CreateMessageReplyDTO, ResultMessageReplyDTO
 from .reaction import MessageReactionDTO
-from .user import UserDTO
+from .report import (
+    AllUsersReportDTO,
+    AVGReportDTO,
+    ChatReportDTO,
+    DailyReportDTO,
+    SingleUserReportDTO,
+)
+from .template_dto import TemplateDTO, TemplateSearchResultDTO, UpdateTemplateTitleDTO
+from .user import CreateUserDTO, UpdateUserDTO, UserDTO
 
 __all__ = [
+    # Activity
+    "CreateActivityDTO",
+    "ResultActivityDTO",
+    # Category
+    "CategoryDTO",
+    "CreateCategoryDTO",
+    "UpdateCategoryDTO",
+    # Chat
     "ChatDTO",
+    "UserChatsDTO",
+    # Daily Activity
     "ChatDailyStatsDTO",
-    "MessageReactionDTO",
     "PopularReactionDTO",
     "UserDailyActivityDTO",
-    "UserDTO",
     "UserReactionActivityDTO",
+    # Message
+    "CreateMessageDTO",
+    "ResultMessageDTO",
+    # Message Reply
+    "CreateMessageReplyDTO",
+    "ResultMessageReplyDTO",
+    # Reaction
+    "MessageReactionDTO",
+    # Report
+    "AVGReportDTO",
+    "AllUsersReportDTO",
+    "ChatReportDTO",
+    "DailyReportDTO",
+    "SingleUserReportDTO",
+    # Template
+    "TemplateDTO",
+    "TemplateSearchResultDTO",
+    "UpdateTemplateTitleDTO",
+    # User
+    "CreateUserDTO",
+    "UpdateUserDTO",
+    "UserDTO",
 ]
