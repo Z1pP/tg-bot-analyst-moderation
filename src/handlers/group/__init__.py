367900fe48be47e5223c5bd029e897300c231275
Z1pP <maxm_99@mail.ru>
2025-09-30 00:51:15 +0300

    remove moderation_router

--- src/handlers/group/__init__.py
+++ src/handlers/group/__init__.py
@@ -1,6 +1,6 @@
 from aiogram import Router
 
-from .ban_user import router as banhammer_router
+from .moderation import router as moderation_router
 from .chat_tracking_handler import router as chat_tracking_router
 from .message_handler import router as message_router
 from .new_members import router as new_members_router
@@ -11,6 +11,6 @@ router = Router(name="group_router")
 router.include_router(new_members_router)
 router.include_router(inline_query_router)
 router.include_router(chat_tracking_router)
-router.include_router(banhammer_router)
+router.include_router(moderation_router)
 router.include_router(message_router)
 router.include_router(reactions_router)
