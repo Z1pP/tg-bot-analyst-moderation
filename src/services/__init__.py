af85fdfb90ca3160ad37c52f3ba8182764e6412a
Z1pP <maxm_99@mail.ru>
2025-09-29 20:24:33 +0300

    added ChatService, BotMessageService, UserService, PunishmentService to init

---
+++ src/services/__init__.py
@@ -0,0 +1,11 @@
+from .chat import ChatService
+from .messaging import BotMessageService
+from .punishment_service import PunishmentService
+from .user import UserService
+
+__all__ = [
+    "ChatService",
+    "BotMessageService",
+    "UserService",
+    "PunishmentService",
+]
