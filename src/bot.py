66ea5b301b14722bc9cee472c5167fbd34b9952a
Z1pP <maxm_99@mail.ru>
2025-09-29 20:10:11 +0300

    refactoring

--- src/bot.py
+++ src/bot.py
@@ -1,27 +1,17 @@
 from aiogram import Bot, Dispatcher
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
-from aiogram.fsm.storage.memory import MemoryStorage
 
 from callbacks import register_callback_routers
 from commands import start_commands
-from config import settings
+from di import container
 from handlers import registry_routers
 
 
-async def init_bot() -> tuple[Bot, Dispatcher]:
-    print(settings.model_dump())
-    bot = Bot(
-        token=settings.BOT_TOKEN,
-        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
-    )
-
-    # Создаем хранилище
-    storage = MemoryStorage()
+async def configure_dispatcher() -> tuple[Bot, Dispatcher]:
+    bot = container.resolve(Bot)
+    dp = container.resolve(Dispatcher)
 
     await start_commands.set_bot_commands(bot=bot)
 
-    dp = Dispatcher(storage=storage)
     registry_routers(dispatcher=dp)
     register_callback_routers(dispatcher=dp)
 
