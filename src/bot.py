d6843427bc09e39047bd07936442fb81dfff364c
Z1pP <maxm_99@mail.ru>
2025-10-08 22:42:33 +0300

    added exception registration

--- src/bot.py
+++ src/bot.py
@@ -4,15 +4,17 @@ from callbacks import register_callback_routers
 from commands import start_commands
 from di import container
 from handlers import registry_routers
+from utils.exception_handler import registry_exceptions
 
 
 async def configure_dispatcher() -> tuple[Bot, Dispatcher]:
-    bot = container.resolve(Bot)
-    dp = container.resolve(Dispatcher)
+    bot: Bot = container.resolve(Bot)
+    dp: Dispatcher = container.resolve(Dispatcher)
 
     await start_commands.set_bot_commands(bot=bot)
 
     registry_routers(dispatcher=dp)
     register_callback_routers(dispatcher=dp)
+    registry_exceptions(dispatcher=dp)
 
     return bot, dp
