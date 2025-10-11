6010fddf6f1da603123b042947e9c224d3c4b84a
Z1pP <maxm_99@mail.ru>
2025-10-11 12:35:53 +0300

    created DatabaseContextManager

--- src/database/session.py
+++ src/database/session.py
@@ -1,4 +1,6 @@
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from contextlib import asynccontextmanager
+
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from config import settings
 
@@ -11,3 +13,13 @@ engine = create_async_engine(
     echo=False,
 )
 async_session = async_sessionmaker(engine, expire_on_commit=False)
+
+
+class DatabaseContextManager:
+    def __init__(self, session_factory: async_sessionmaker[AsyncSession]) -> None:
+        self._session_factory = session_factory
+
+    @asynccontextmanager
+    async def session(self):
+        async with self._session_factory() as session:
+            yield session
