b157cf54fb94554d3de1b9b5ee113fe2be4ea346
Z1pP <maxm_99@mail.ru>
2025-10-20 23:53:08 +0300

    remove unsuded state fileds in ChatStateManager

--- src/states/chat_states.py
+++ src/states/chat_states.py
@@ -2,9 +2,6 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class ChatStateManager(StatesGroup):
-    report_menu = State()
-
-    listing_chats = State()
     listing_tracking_chats = State()
 
     selecting_chat = State()
@@ -12,5 +9,3 @@ class ChatStateManager(StatesGroup):
 
     selecting_period = State()
     selecting_custom_period = State()
-
-    waiting_for_chat_message = State()
