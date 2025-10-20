484373eab7fb5565818689344766de2f478f2201
Z1pP <maxm_99@mail.ru>
2025-10-20 03:31:21 +0300

    refactor GetChatsWithBannedUserUseCase

--- src/usecases/amnesty/get_chats_with_banned_user.py
+++ src/usecases/amnesty/get_chats_with_banned_user.py
@@ -1,6 +1,5 @@
-from typing import List
-
-from dto import ChatDTO
+from typing import List, Optional
+from dto import ChatDTO, AmnestyUserDTO
 from repositories import ChatTrackingRepository, UserChatStatusRepository
 from services import UserService
 
@@ -16,31 +15,32 @@ class GetChatsWithBannedUserUseCase:
         self.chat_tracking_repository = chat_tracking_repository
         self.user_chat_status_repository = user_chat_status_repository
 
-    async def execute(self, admin_tgid: str, violator_tgid: str) -> List[ChatDTO]:
-        admin = await self.user_service.get_user(tg_id=admin_tgid)
-        violator = await self.user_service.get_user(tg_id=violator_tgid)
+    async def execute(self, dto: AmnestyUserDTO) -> Optional[List[ChatDTO]]:
+        admin = await self.user_service.get_user(tg_id=dto.admin_tgid)
 
-        if not admin or not violator:
-            return []
-
+        # Получаем все отслеживаемые чаты администратора
         tracked_chats = await self.chat_tracking_repository.get_all_tracked_chats(
             admin_id=admin.id
         )
 
-        result = []
+        # Среди отслеживаемых чатов администратора ищем где пользователь заблокирован
+        chat_dtos = []
         for chat in tracked_chats:
             status = await self.user_chat_status_repository.get_status(
-                user_id=violator.id,
+                user_id=dto.violator_id,
                 chat_id=chat.id,
             )
 
             if status and status.is_banned:
-                result.append(
+                chat_dtos.append(
                     ChatDTO(
                         id=chat.id,
-                        chat_id=chat.chat_id,
+                        tg_id=chat.chat_id,
                         title=chat.title,
                     )
                 )
 
-        return result
+        if not chat_dtos:
+            return None
+
+        return chat_dtos
