45f32667f55046b4d3c4ac804f453e9f2ffcdaa0
Z1pP <maxm_99@mail.ru>
2025-10-21 00:17:10 +0300

    remove help router

--- src/handlers/private/common/__init__.py
+++ src/handlers/private/common/__init__.py
@@ -1,6 +1,5 @@
 from aiogram import Router
 
-from .help_handler import router as help_router
 from .menu_handler import router as menu_router
 from .start_handler import router as start_router
 from .time_router import router as local_time_router
@@ -8,5 +7,4 @@ from .time_router import router as local_time_router
 router = Router(name="common_router")
 router.include_router(menu_router)
 router.include_router(start_router)
-router.include_router(help_router)
 router.include_router(local_time_router)
