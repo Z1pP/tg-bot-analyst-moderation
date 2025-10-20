6523603bb3d528d06256bcf1149abf736baf18d3
Z1pP <maxm_99@mail.ru>
2025-10-20 23:53:46 +0300

    remove unused state fileds in UsernameStates

--- src/states/user_states.py
+++ src/states/user_states.py
@@ -1,22 +1,6 @@
 from aiogram.fsm.state import State, StatesGroup
 
 
-class UserStateManager(StatesGroup):
-    """Состояния для управления пользователями."""
-
-    selecting_user = State()
-    viewing_user = State()
-    editing_user = State()
-
-    # Состояния для работы с отчетами для выбранного пользователя
-    report_menu = State()
-    process_select_time_period = State()
-    report_full_selecting_period = State()
-
-    process_custom_period_input = State()
-    report_full_waiting_input_period = State()
-
-
 class UsernameStates(StatesGroup):
     """Состояние для задавания имени пользователя"""
 
@@ -27,7 +11,6 @@ class SingleUserReportStates(StatesGroup):
     selected_single_user = State()
     selecting_period = State()
     waiting_cutom_period = State()
-    order_details_report = State()
 
 
 class AllUsersReportStates(StatesGroup):
