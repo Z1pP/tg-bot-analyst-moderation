9f0f91e95274292c1e0665417b7f6ea1342485dd
Z1pP <maxm_99@mail.ru>
2025-10-23 18:03:34 +0300

    refactoring

--- src/usecases/user_tracking/get_list_tracked_user.py
+++ src/usecases/user_tracking/get_list_tracked_user.py
@@ -6,7 +6,7 @@ class GetListTrackedUsersUseCase:
     def __init__(self, user_tracking_repository: UserTrackingRepository):
         self._user_tracking_repository = user_tracking_repository
 
-    async def execute(self, admin_username: str) -> list[User]:
+    async def execute(self, admin_tgid: str) -> list[User]:
         return await self._user_tracking_repository.get_tracked_users_by_admin(
-            admin_username=admin_username
+            admin_tgid=admin_tgid
         )
--- src/usecases/user_tracking/remove_user_from_tracking.py
+++ src/usecases/user_tracking/remove_user_from_tracking.py
@@ -1,12 +1,29 @@
+from dto import RemoveUserTrackingDTO
 from repositories import UserTrackingRepository
+from services import UserService
 
 
 class RemoveUserFromTrackingUseCase:
-    def __init__(self, user_tracking_repository: UserTrackingRepository):
+    def __init__(
+        self,
+        user_tracking_repository: UserTrackingRepository,
+        user_service: UserService,
+    ):
         self.user_tracking_repository = user_tracking_repository
+        self.user_service = user_service
 
-    async def execute(self, admin_username: str, user_username: str) -> bool:
+    async def execute(self, dto: RemoveUserTrackingDTO) -> bool:
         """Удаляет пользователя из списка отслеживания админа."""
-        return await self.user_tracking_repository.remove_user_from_tracking(
-            admin_username=admin_username, user_username=user_username
+        admin = await self.user_service.get_user(
+            tg_id=dto.admin_tgid,
+            username=dto.admin_username,
         )
+
+        if not admin:
+            return False
+
+        await self.user_tracking_repository.remove_user_from_tracking(
+            admin_id=admin.id,
+            user_id=dto.user_id,
+        )
+        return True
