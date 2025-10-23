570544d4f5904a02098a93cc4847700911fa8c02
Z1pP <maxm_99@mail.ru>
2025-10-23 20:12:46 +0300

    added block_user_router

--- src/handlers/private/banhammer/__init__.py
+++ src/handlers/private/banhammer/__init__.py
@@ -2,8 +2,10 @@ from aiogram import Router
 
 from .amnesty import router as amnesty_router
 from .lock_menu import router as lock_menu_router
+from .block_user import router as block_user_router
 
 router = Router(name="banhammer_router")
 
 router.include_router(lock_menu_router)
 router.include_router(amnesty_router)
+router.include_router(block_user_router)
