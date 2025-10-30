6ea565f9b44d94c0cff5cbb3380bcdb236fe6b54
Z1pP <maxm_99@mail.ru>
2025-10-30 16:21:43 +0300

    remove

--- src/states/admin_message_actions_states.py
+++
@@ -1,9 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-
-class AdminMessageActionsStates(StatesGroup):
-    """Состояния для работы с сообщениями через админ-панель."""
-
-    waiting_action_select = State()
-    waiting_delete_confirm = State()
-    waiting_reply_message = State()
