effff0a610e01b560a2e3657c80fe2ca225ae7a4
Z1pP <maxm_99@mail.ru>
2025-10-30 17:22:12 +0300

    added inline keyboard send_message_ikb

--- src/handlers/private/message_management/message_manager.py
+++ src/handlers/private/message_management/message_manager.py
@@ -1,10 +1,11 @@
 import logging
 from aiogram import Router, F, types
 from aiogram.fsm.context import FSMContext
-from states import MessageManagerState
-from utils.state_logger import log_and_set_state
 
 from constants import KbCommands, Dialog
+from keyboards.inline.message_actions import send_message_ikb
+from states import MessageManagerState
+from utils.state_logger import log_and_set_state
 
 
 router = Router(name=__name__)
@@ -19,5 +20,8 @@ async def message_management_handler(message: types.Message, state: FSMContext) 
         KbCommands.MESSAGE_MANAGEMENT,
     )
 
-    await message.answer(text=Dialog.MessageManager.INPUT_MESSAGE_LINK)
+    await message.answer(
+        text=Dialog.MessageManager.INPUT_MESSAGE_LINK,
+        reply_markup=send_message_ikb(),
+    )
     await log_and_set_state(message, state, MessageManagerState.waiting_message_link)
