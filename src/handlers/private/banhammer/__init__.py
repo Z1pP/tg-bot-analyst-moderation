5bf038eb35d1a6f16ce219cd1eef673d61668274
Z1pP <maxm_99@mail.ru>
2025-10-31 00:44:57 +0300

    added warn_user_router

--- src/handlers/private/banhammer/__init__.py
+++ src/handlers/private/banhammer/__init__.py
@@ -3,9 +3,11 @@ from aiogram import Router
 from .amnesty import router as amnesty_router
 from .lock_menu import router as lock_menu_router
 from .block_user import router as block_user_router
+from .warn_user import router as warn_user_router
 
 router = Router(name="banhammer_router")
 
 router.include_router(lock_menu_router)
 router.include_router(amnesty_router)
 router.include_router(block_user_router)
+router.include_router(warn_user_router)
