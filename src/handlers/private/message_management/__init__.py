9972026ac95dbf53d1b6786925ad639a1d57f2f1
Z1pP <maxm_99@mail.ru>
2025-10-30 16:27:25 +0300

    create init

--- src/handlers/private/message_management/__init__.py
+++ src/handlers/private/message_management/__init__.py
@@ -1,5 +1,17 @@
 from aiogram import Router
-from .message_manager import router as message_router
 
-router = Router(name=__name__)
-router.include_router(message_router)
+from .action_select_handler import router as action_select_router
+from .delete_handler import router as delete_router
+from .message_link_handler import router as message_link_router
+from .reply_handler import router as reply_router
+from .message_manager import router as message_management_router
+
+router = Router()
+
+router.include_router(message_management_router)
+router.include_router(message_link_router)
+router.include_router(action_select_router)
+router.include_router(delete_router)
+router.include_router(reply_router)
+
+__all__ = ["router"]
