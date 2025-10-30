e19e25efd16f5738893c6c34af0b439533a8d229
Z1pP <maxm_99@mail.ru>
2025-10-31 00:47:08 +0300

    remove BanUserFromAdminPanelUseCase

--- src/usecases/moderation/__init__.py
+++ src/usecases/moderation/__init__.py
@@ -1,9 +1,7 @@
-from .ban_user_from_admin_panel import BanUserFromAdminPanelUseCase
 from .give_ban_user import GiveUserBanUseCase
 from .give_warn_user import GiveUserWarnUseCase
 
 __all__ = [
     "GiveUserWarnUseCase",
     "GiveUserBanUseCase",
-    "BanUserFromAdminPanelUseCase",
 ]
