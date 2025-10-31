0e6ddb3dd9104d90ceb0463162a780bdb30b8c37
Z1pP <maxm_99@mail.ru>
2025-10-31 16:16:38 +0300

    remove waiting_confirmation_action

--- src/states/banhammer_states.py
+++ src/states/banhammer_states.py
@@ -16,7 +16,6 @@ class BanUserStates(StatesGroup):
     waiting_user_input = State()
     waiting_reason_input = State()
     waiting_chat_select = State()
-    waiting_confirmation_action = State()
 
 
 class WarnUserStates(StatesGroup):
