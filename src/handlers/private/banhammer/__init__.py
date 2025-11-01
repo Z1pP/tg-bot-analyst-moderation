8a3e1434e3989b2bb33907266d09dd1bdf0b200c
Z1pP <maxm_99@mail.ru>
2025-11-01 22:05:42 +0300

    added back_to_block_menu_router

--- src/handlers/private/banhammer/__init__.py
+++ src/handlers/private/banhammer/__init__.py
@@ -1,12 +1,15 @@
 from aiogram import Router
 
+
 from .amnesty import router as amnesty_router
 from .lock_menu import router as lock_menu_router
 from .block_user import router as block_user_router
 from .warn_user import router as warn_user_router
+from .back_to_block_menu import router as back_to_block_menu_router
 
 router = Router(name="banhammer_router")
 
+router.include_router(back_to_block_menu_router)
 router.include_router(lock_menu_router)
 router.include_router(amnesty_router)
 router.include_router(block_user_router)
