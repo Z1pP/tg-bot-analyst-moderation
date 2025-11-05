5317b14d0b2f73bf01afceaa05bd23807f18443e
Z1pP <maxm_99@mail.ru>
2025-11-05 14:08:25 +0300

    templates_menu_handler is callback now

--- src/handlers/private/templates/templates_menu.py
+++ src/handlers/private/templates/templates_menu.py
@@ -1,24 +1,20 @@
-from aiogram import F, Router
-from aiogram.fsm.context import FSMContext
-from aiogram.types import Message
+from aiogram import F, Router, types
 
-from constants import KbCommands
 from keyboards.reply.menu import tamplates_menu_kb
 from utils.send_message import send_html_message_with_kb
 
 router = Router(name=__name__)
 
 
-@router.message(
-    F.text == KbCommands.TEMPLATES_MENU,
-)
-async def templates_menu_handler(message: Message, state: FSMContext):
+@router.callback_query(F.data == "templates_menu")
+async def templates_menu_handler(callback: types.Message):
     """Обработчик меню шаблонов"""
+    await callback.answer()
 
     message_text = "Выбери действие:"
 
     await send_html_message_with_kb(
-        message=message,
+        message=callback.message,
         text=message_text,
         reply_markup=tamplates_menu_kb(),
     )
