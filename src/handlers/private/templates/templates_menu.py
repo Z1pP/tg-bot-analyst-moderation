9c5e77685b863b5a64054a762abd7bfee0cefc5b
Z1pP <maxm_99@mail.ru>
2025-11-06 02:26:59 +0300

    refactoring

--- src/handlers/private/templates/templates_menu.py
+++ src/handlers/private/templates/templates_menu.py
@@ -1,20 +1,18 @@
 from aiogram import F, Router, types
 
-from keyboards.reply.menu import tamplates_menu_kb
-from utils.send_message import send_html_message_with_kb
+from keyboards.inline.templates import templates_menu_ikb
 
 router = Router(name=__name__)
 
 
 @router.callback_query(F.data == "templates_menu")
-async def templates_menu_handler(callback: types.Message):
+async def templates_menu_handler(callback: types.CallbackQuery):
     """Обработчик меню шаблонов"""
     await callback.answer()
 
     message_text = "Выбери действие:"
 
-    await send_html_message_with_kb(
-        message=callback.message,
+    await callback.message.edit_text(
         text=message_text,
-        reply_markup=tamplates_menu_kb(),
+        reply_markup=templates_menu_ikb(),
     )
