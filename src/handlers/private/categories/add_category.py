800ec3ff801e040168db4abb6bded3d4d8e406c5
Z1pP <maxm_99@mail.ru>
2025-11-12 06:29:26 +0300

    remove add_category_handler and process_category_name_handler handlers

--- src/handlers/private/categories/add_category.py
+++ src/handlers/private/categories/add_category.py
@@ -6,10 +6,6 @@ from aiogram.fsm.context import FSMContext
 from container import container
 from dto import CreateCategoryDTO
 from exceptions.category import CategoryAlreadyExists
-from keyboards.inline.categories import (
-    cancel_category_ikb,
-    confirmation_add_category_ikb,
-)
 from keyboards.inline.templates import templates_menu_ikb
 from states import CategoryStateManager, TemplateStateManager
 from usecases.categories import CreateCategoryUseCase
@@ -18,101 +14,6 @@ from utils.state_logger import log_and_set_state
 
 router = Router(name=__name__)
 logger = logging.getLogger(__name__)
-
-
-@router.callback_query(
-    F.data == "add_category",
-    TemplateStateManager.templates_menu,
-)
-async def add_category_handler(
-    callback: types.CallbackQuery,
-    state: FSMContext,
-    bot: Bot,
-) -> None:
-    """Обработчик добавления новой категории"""
-    await callback.answer()
-
-    sent_success = await safe_edit_message(
-        bot,
-        chat_id=callback.message.chat.id,
-        message_id=callback.message.message_id,
-        text="Отправьте название новой категории:",
-        reply_markup=cancel_category_ikb(),
-    )
-
-    if sent_success:
-        await state.update_data(active_message_id=callback.message.message_id)
-
-    await log_and_set_state(
-        message=callback.message,
-        state=state,
-        new_state=CategoryStateManager.process_category_name,
-    )
-
-
-@router.message(CategoryStateManager.process_category_name)
-async def process_category_name_handler(
-    message: types.Message,
-    state: FSMContext,
-    bot: Bot,
-) -> None:
-    """Обработчик получения названия категории"""
-    data = await state.get_data()
-    active_message_id = data.get("active_message_id")
-
-    # Проверяем формат ввода
-    if not message.text:
-        if active_message_id:
-            await safe_edit_message(
-                bot,
-                chat_id=message.chat.id,
-                message_id=active_message_id,
-                text=(
-                    "❗Недопустимый формат ввода данных.\n"
-                    "Пожалуйста, отправьте название текстом (без фото, видео и т.д.)."
-                ),
-                reply_markup=cancel_category_ikb(),
-            )
-        return
-
-    category_name = message.text.strip()
-    await message.delete()
-
-    # Валидация длины названия
-    if not validate_category_name(category_name):
-        if active_message_id:
-            await safe_edit_message(
-                bot,
-                chat_id=message.chat.id,
-                message_id=active_message_id,
-                text=(
-                    "❗Недопустимый формат ввода данных.\n"
-                    "Название категории должно содержать от 3 до 50 символов."
-                ),
-                reply_markup=cancel_category_ikb(),
-            )
-        return
-
-    # Подтверждение создания
-    if active_message_id:
-        await safe_edit_message(
-            bot,
-            chat_id=message.chat.id,
-            message_id=active_message_id,
-            text=(
-                f"Категория <b>{category_name}</b>.\n"
-                "Вы подтверждаете создание категории?"
-            ),
-            reply_markup=confirmation_add_category_ikb(),
-        )
-
-    await state.update_data(category_name=category_name)
-
-    await log_and_set_state(
-        message=message,
-        state=state,
-        new_state=CategoryStateManager.confirm_category_creation,
-    )
 
 
 @router.callback_query(
@@ -156,8 +57,3 @@ async def confirm_category_creation_handler(
         state=state,
         new_state=TemplateStateManager.templates_menu,
     )
-
-
-def validate_category_name(category_name: str) -> bool:
-    """Валидирует название категории"""
-    return 3 <= len(category_name) <= 50
