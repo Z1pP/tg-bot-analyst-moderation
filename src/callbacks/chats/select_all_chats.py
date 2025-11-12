5f9b3feb35715eeff3394acea51751f7e5c1f593
Z1pP <maxm_99@mail.ru>
2025-11-13 01:17:59 +0300

    integrated state logging into all_chats_handler to enhance state management and error tracking

--- src/callbacks/chats/select_all_chats.py
+++ src/callbacks/chats/select_all_chats.py
@@ -6,6 +6,7 @@ from keyboards.reply import chat_actions_kb
 from states import ChatStateManager
 from utils.exception_handler import handle_exception
 from utils.send_message import send_html_message_with_kb
+from utils.state_logger import log_and_set_state
 
 router = Router(name=__name__)
 
@@ -16,13 +17,18 @@ async def all_chats_handler(callback: CallbackQuery, state: FSMContext) -> None:
     Обработчик команды для получения списка всех чатов.
     """
     try:
-        await state.set_state(ChatStateManager.selecting_all_chats)
         await state.update_data(chat_title="all")
 
         await send_html_message_with_kb(
             message=callback.message,
             text="Выбрано: все чаты",
             reply_markup=chat_actions_kb(),
+        )
+
+        await log_and_set_state(
+            message=callback.message,
+            state=state,
+            new_state=ChatStateManager.selecting_all_chats,
         )
 
     except Exception as e:
