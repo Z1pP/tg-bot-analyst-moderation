f4a33148b8f06fd8683fe25facb70acd3daa1c9a
Z1pP <maxm_99@mail.ru>
2025-11-12 04:18:32 +0300

    added add_category_router

--- src/handlers/private/categories/__init__.py
+++ src/handlers/private/categories/__init__.py
@@ -1,5 +1,6 @@
 from aiogram import Router
 
+from .add_category import router as add_category_router
 from .delete_category import router as delete_category_router
 from .edit_category import router as edit_category_router
 from .list_categories import router as list_categories_router
@@ -7,6 +8,7 @@ from .pagination import router as pagination_router
 from .select_category import router as select_category_router
 
 router = Router(name="categories_router")
+router.include_router(add_category_router)
 router.include_router(select_category_router)
 router.include_router(delete_category_router)
 router.include_router(edit_category_router)
