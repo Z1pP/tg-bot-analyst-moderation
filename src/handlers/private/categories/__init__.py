5438550b6c659ad54ff3edc652f2df8639d64df2
Z1pP <maxm_99@mail.ru>
2025-11-12 06:10:01 +0300

    added common_router

--- src/handlers/private/categories/__init__.py
+++ src/handlers/private/categories/__init__.py
@@ -1,6 +1,7 @@
 from aiogram import Router
 
 from .add_category import router as add_category_router
+from .common import router as common_router
 from .delete_category import router as delete_category_router
 from .edit_category import router as edit_category_router
 from .list_categories import router as list_categories_router
@@ -14,3 +15,4 @@ router.include_router(delete_category_router)
 router.include_router(edit_category_router)
 router.include_router(pagination_router)
 router.include_router(list_categories_router)
+router.include_router(common_router)
