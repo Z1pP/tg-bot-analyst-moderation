2b2e44a9332d2ca728fc24d45ec06ee5187e1c07
Z1pP <maxm_99@mail.ru>
2025-11-12 05:22:10 +0300

    added editing_category_name and removing_category states

--- src/states/category.py
+++ src/states/category.py
@@ -4,3 +4,5 @@ from aiogram.fsm.state import State, StatesGroup
 class CategoryStateManager(StatesGroup):
     process_category_name = State()
     confirm_category_creation = State()
+    editing_category_name = State()
+    removing_category = State()
