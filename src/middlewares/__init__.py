79aa5af662c26b7badbf48c39a6315a77335dd6d
Z1pP <maxm_99@mail.ru>
2025-11-14 16:15:51 +0300

    added AdminAntispamMiddleware to the middleware exports, improving spam protection capabilities

--- src/middlewares/__init__.py
+++ src/middlewares/__init__.py
@@ -1,3 +1,4 @@
+from .admin_antispam import AdminAntispamMiddleware
 from .album_middleware import AlbumMiddleware
 
-__all__ = ["AlbumMiddleware"]
+__all__ = ["AdminAntispamMiddleware", "AlbumMiddleware"]
