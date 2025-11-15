c13b19266133553ec6448e282f52201436ca1017
Z1pP <maxm_99@mail.ru>
2025-11-15 15:57:08 +0300

    refactored admin and public router initialization in registry_admin_routers and registry_group_routers functions for clarity and consistency, renaming variables to better reflect their purpose

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -12,33 +12,33 @@ from .private import router as private_router
 
 def registry_admin_routers(dispatcher: Dispatcher):
     # Создаем роутер для админов
-    admin_router = Router(name="admin_router")
+    only_admin_router = Router(name="admin_router")
 
     # Регистрируем фильтры: только для приватных чатов и только для админов
-    admin_router.message.filter(
+    only_admin_router.message.filter(
         ChatTypeFilter(chat_type=[ChatType.PRIVATE]),
         AdminOnlyFilter(),
     )
 
     # Регистрируем антиспам middleware
     cache: ICache = container.resolve(ICache)
-    admin_router.message.middleware(AdminAntispamMiddleware(cache))
+    only_admin_router.message.middleware(AdminAntispamMiddleware(cache))
 
     # Регистрируем приватный роутер
-    admin_router.include_router(private_router)
+    only_admin_router.include_router(private_router)
 
     # Регистрируем роутер для админов
-    dispatcher.include_router(admin_router)
+    dispatcher.include_router(only_admin_router)
 
 
 def registry_group_routers(dispatcher: Dispatcher):
     # Регистриуем групповой роутер
-    staff_router = Router(name="staff_router")
-    staff_router.message.filter(GroupTypeFilter())
+    public_router = Router(name="public_router")
+    public_router.message.filter(GroupTypeFilter())
 
-    staff_router.include_router(group_router)
+    public_router.include_router(group_router)
 
-    dispatcher.include_router(staff_router)
+    dispatcher.include_router(public_router)
 
 
 def registry_routers(dispatcher: Dispatcher):
