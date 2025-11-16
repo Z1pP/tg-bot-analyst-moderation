19ab20cf5348f766057a8981034b76634c86e63b
Z1pP <maxm_99@mail.ru>
2025-11-16 05:46:08 +0300

    removed unused callback router registration from the dispatcher configuration, further simplifying the bot's structure and enhancing maintainability

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,5 @@
 from aiogram import Bot, Dispatcher
 
-from callbacks import register_callback_routers
 from di import container
 from handlers import registry_routers
 from utils.exception_handler import registry_exceptions
@@ -11,7 +10,6 @@ async def configure_dispatcher() -> tuple[Bot, Dispatcher]:
     dp: Dispatcher = container.resolve(Dispatcher)
 
     registry_routers(dispatcher=dp)
-    register_callback_routers(dispatcher=dp)
     registry_exceptions(dispatcher=dp)
 
     return bot, dp
