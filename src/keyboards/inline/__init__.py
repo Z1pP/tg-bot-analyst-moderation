b037b589c0605b7f21bb73132314e0fb7661fafa
Z1pP <maxm_99@mail.ru>
2025-11-16 04:32:26 +0300

    updated inline keyboard exports in __init__.py to include new user management buttons; enhanced user navigation options with cancel and menu buttons

--- src/keyboards/inline/__init__.py
+++ src/keyboards/inline/__init__.py
@@ -1,10 +1,17 @@
 from .calendar_kb import CalendarKeyboard
 from .report import order_details_kb
-from .users import remove_user_inline_kb, users_inline_kb
+from .users import (
+    cancel_add_user_ikb,
+    remove_user_inline_kb,
+    users_inline_kb,
+    users_menu_ikb,
+)
 
 __all__ = [
     "users_inline_kb",
     "remove_user_inline_kb",
+    "users_menu_ikb",
+    "cancel_add_user_ikb",
     "order_details_kb",
     "CalendarKeyboard",
 ]
