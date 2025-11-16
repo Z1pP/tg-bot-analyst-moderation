be8921cf846af6f7878b182a359ed8a5441b0bff
Z1pP <maxm_99@mail.ru>
2025-11-16 05:33:52 +0300

    removed users router from callback registration in __init__.py to streamline navigation and improve user experience

--- src/callbacks/__init__.py
+++ src/callbacks/__init__.py
@@ -2,10 +2,8 @@ from aiogram import Dispatcher
 
 from .chats import router as chats_router
 from .reports import router as reports_router
-from .users import router as users_router
 
 
 def register_callback_routers(dispatcher: Dispatcher):
-    dispatcher.include_router(users_router)
     dispatcher.include_router(chats_router)
     dispatcher.include_router(reports_router)
