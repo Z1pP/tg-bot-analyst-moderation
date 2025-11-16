d90aae81324b5dc060e310eaac962c18ca5cd7a4
Z1pP <maxm_99@mail.ru>
2025-11-16 04:31:50 +0300

    removed user_menu_kb from exports in __init__.py to streamline keyboard management; updated __all__ for improved clarity and organization

--- src/keyboards/reply/__init__.py
+++ src/keyboards/reply/__init__.py
@@ -1,5 +1,5 @@
 from .chat_actions import chat_actions_kb
-from .menu import admin_menu_kb, chat_menu_kb, get_back_kb, user_menu_kb
+from .menu import admin_menu_kb, chat_menu_kb, get_back_kb
 from .time_period import get_time_period_for_full_report, get_time_period_kb
 
 __all__ = [
@@ -7,7 +7,6 @@ __all__ = [
     "admin_menu_kb",
     "get_time_period_for_full_report",
     "get_back_kb",
-    "user_menu_kb",
     "chat_actions_kb",
     "chat_menu_kb",
 ]
