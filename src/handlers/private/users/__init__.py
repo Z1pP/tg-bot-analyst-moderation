7f179ae6f5f5ebcdce085b2a4341f16d17b375db
Z1pP <maxm_99@mail.ru>
2025-11-16 04:30:56 +0300

    added new routers for user menu navigation and pagination in users module; improved organization of user-related routes for better structure

--- src/handlers/private/users/__init__.py
+++ src/handlers/private/users/__init__.py
@@ -1,10 +1,16 @@
 from aiogram import Router
 
 from .add_user_to_tracking import router as add_router
+from .back_to_users_menu import router as back_router
 from .delete_user_from_tracking import router as delete_router
 from .list_tracked_users import router as list_router
+from .pagination import router as pagination_router
+from .users_menu import router as menu_router
 
 router = Router(name="users_router")
+router.include_router(menu_router)
+router.include_router(back_router)
 router.include_router(list_router)
 router.include_router(add_router)
 router.include_router(delete_router)
+router.include_router(pagination_router)
