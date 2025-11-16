7c2a9e17fe00e2812865d53e65e1eb494496a520
Z1pP <maxm_99@mail.ru>
2025-11-16 05:36:05 +0300

    added handler for retrieving all users, implementing state management and error handling to enhance user interaction and navigation within the user management interface

---
+++ src/handlers/private/users/select_all_users.py
@@ -0,0 +1,34 @@
+from aiogram import F, Router
+from aiogram.fsm.context import FSMContext
+from aiogram.types import CallbackQuery
+
+from keyboards.inline.users import all_users_actions_ikb
+from states import AllUsersReportStates
+from utils.exception_handler import handle_exception
+from utils.state_logger import log_and_set_state
+
+router = Router(name=__name__)
+
+
+@router.callback_query(F.data == "all_users")
+async def all_users_handler(callback: CallbackQuery, state: FSMContext) -> None:
+    """
+    Обработчик команды для получения списка всех пользователей.
+    """
+    try:
+        await callback.answer()
+        await log_and_set_state(
+            message=callback.message,
+            state=state,
+            new_state=AllUsersReportStates.selected_all_users,
+        )
+        await callback.message.edit_text(
+            text="Выбери действие:",
+            reply_markup=all_users_actions_ikb(),
+        )
+
+    except Exception as e:
+        await handle_exception(
+            message=callback.message, exc=e, context="all_users_handler"
+        )
+        await state.clear()
