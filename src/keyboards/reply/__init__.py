db674e76501e74a63bbe052af84ae3cf59d76a7f
Z1pP <maxm_99@mail.ru>
2025-11-17 19:33:39 +0300

    Refactor keyboard exports in reply module to streamline imports. Removed unused keyboard functions for improved clarity and maintainability.

--- src/keyboards/reply/__init__.py
+++ src/keyboards/reply/__init__.py
@@ -1,12 +1,5 @@
-from .chat_actions import chat_actions_kb
-from .menu import admin_menu_kb, chat_menu_kb, get_back_kb
-from .time_period import get_time_period_for_full_report, get_time_period_kb
+from .menu import admin_menu_kb
 
 __all__ = [
-    "get_time_period_kb",
     "admin_menu_kb",
-    "get_time_period_for_full_report",
-    "get_back_kb",
-    "chat_actions_kb",
-    "chat_menu_kb",
 ]
