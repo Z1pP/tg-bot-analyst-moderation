355a4930e5495415a301f90e36dcd7ea1439bd01
Z1pP <maxm_99@mail.ru>
2025-11-17 04:03:09 +0300

    Refactor report handlers to update module imports, improving code organization and clarity. Renamed single chat report module and added chat report module for better structure.

--- src/handlers/private/reports/__init__.py
+++ src/handlers/private/reports/__init__.py
@@ -1,11 +1,11 @@
 from aiogram import Router
 
-from .all_users_report import router as all_users_report_router
+from .all_users import router as all_users_report_router
 from .back_handler import router as back_router
 from .calendar_callback_handler import router as calendar_router
+from .chat import router as chat_report_router
 from .detailed_report import router as detailed_report_router
-from .single_chat_report import router as chat_report_router
-from .single_user_report import router as single_user_report_router
+from .single_user import router as single_user_report_router
 
 router = Router(name="report_router")
 router.include_router(back_router)
