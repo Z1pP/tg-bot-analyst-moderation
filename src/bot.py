8f3faa7d20d2e5a8a9a622d72f06429056e36fbf
Z1pP <maxm_99@mail.ru>
2025-11-17 04:02:38 +0300

    Integrate LanguageMiddleware and UserService into dispatcher configuration, enhancing message handling capabilities.

--- src/bot.py
+++ src/bot.py
@@ -2,12 +2,19 @@ from aiogram import Bot, Dispatcher
 
 from di import container
 from handlers import registry_routers
+from middlewares import LanguageMiddleware
+from services.user import UserService
 from utils.exception_handler import registry_exceptions
 
 
 async def configure_dispatcher() -> tuple[Bot, Dispatcher]:
     bot: Bot = container.resolve(Bot)
     dp: Dispatcher = container.resolve(Dispatcher)
+
+    user_service: UserService = container.resolve(UserService)
+    language_middleware = LanguageMiddleware(user_service)
+    dp.message.middleware(language_middleware)
+    dp.callback_query.middleware(language_middleware)
 
     registry_routers(dispatcher=dp)
     registry_exceptions(dispatcher=dp)
