887d8c5b5777b8c1d6827921f88bf1ec0bdf8637
Z1pP <maxm_99@mail.ru>
2025-11-18 06:35:43 +0300

    Integrate role handler into user management router, enhancing role management capabilities within the user interactions.

--- src/handlers/private/users/__init__.py
+++ src/handlers/private/users/__init__.py
@@ -5,6 +5,7 @@ from .back_to_users_menu import router as back_router
 from .delete_user_from_tracking import router as delete_router
 from .list_tracked_users import router as list_router
 from .pagination import router as pagination_router
+from .role_handler import router as role_router
 from .select_all_users import router as select_all_router
 from .select_specific_user import router as select_specific_router
 from .users_menu import router as menu_router
@@ -18,3 +19,4 @@ router.include_router(delete_router)
 router.include_router(pagination_router)
 router.include_router(select_all_router)
 router.include_router(select_specific_router)
+router.include_router(role_router)
