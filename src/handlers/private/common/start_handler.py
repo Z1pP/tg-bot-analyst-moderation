574323fe6aefdb0e5dc25f89a2a1f4e8f7457826
Z1pP <maxm_99@mail.ru>
2025-11-18 05:55:20 +0300

    Refactor start handler to retrieve user language from the database and update keyboard usage, enhancing user experience and maintainability in menu interactions.

--- src/handlers/private/common/start_handler.py
+++ src/handlers/private/common/start_handler.py
@@ -2,10 +2,11 @@ from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
-
 from constants import Dialog
-from keyboards.reply.menu import admin_menu_kb
-from utils.send_message import send_html_message_with_kb
+from constants.i18n import DEFAULT_LANGUAGE
+from container import container
+from keyboards.inline.menu import admin_menu_ikb
+from services.user import UserService
 
 router = Router(name=__name__)
 
@@ -18,8 +19,14 @@ async def start_handler(message: Message) -> None:
     username = message.from_user.full_name
     welcome_text = Dialog.MENU_TEXT.format(username=username)
 
-    await send_html_message_with_kb(
-        message=message,
+    # Получаем язык пользователя из БД (LanguageMiddleware уже сохранил его)
+    user_service: UserService = container.resolve(UserService)
+    db_user = await user_service.get_user(tg_id=str(message.from_user.id))
+    user_language = (
+        db_user.language if db_user and db_user.language else DEFAULT_LANGUAGE
+    )
+
+    await message.answer(
         text=welcome_text,
-        reply_markup=admin_menu_kb(),
+        reply_markup=admin_menu_ikb(user_language),
     )
