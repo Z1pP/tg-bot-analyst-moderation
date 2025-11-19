c3d3d82d6d699598ef2422cc245194592a86bfd6
Z1pP <maxm_99@mail.ru>
2025-11-19 13:38:51 +0300

    Add admin logs button to inline keyboard for authorized users, enhancing admin menu functionality and navigation.

--- src/keyboards/inline/menu.py
+++ src/keyboards/inline/menu.py
@@ -1,12 +1,17 @@
+from typing import Optional
+
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-from constants import KbCommands
+from constants import RELEASE_NOTES_ADMIN_IDS, KbCommands
 from constants.callback import CallbackData
 from constants.i18n import DEFAULT_LANGUAGE, get_text
 
 
-def admin_menu_ikb(user_language: str = None) -> InlineKeyboardMarkup:
+def admin_menu_ikb(
+    user_language: str = None,
+    admin_tg_id: Optional[str] = None,
+) -> InlineKeyboardMarkup:
     """
     Создает inline клавиатуру главного меню администратора с учетом языка пользователя.
 
@@ -48,11 +53,26 @@ def admin_menu_ikb(user_language: str = None) -> InlineKeyboardMarkup:
         width=2,
     )
 
+    # Добавляем кнопку для просмотра логов действий администраторов
+    # (только для авторизованных пользователей)
+    if admin_tg_id and admin_tg_id in RELEASE_NOTES_ADMIN_IDS:
+        builder.row(
+            InlineKeyboardButton(
+                text=KbCommands.ADMIN_LOGS,
+                callback_data=CallbackData.AdminLogs.MENU,
+            ),
+            InlineKeyboardButton(
+                text=KbCommands.PERMISSIONS_MENU,
+                callback_data=CallbackData.Permissions.MENU,
+            ),
+            width=2,
+        )
+
     builder.row(
         InlineKeyboardButton(
             text=release_notes_text,
             callback_data=CallbackData.ReleaseNotes.MENU,
-        )
+        ),
     )
 
     return builder.as_markup()
