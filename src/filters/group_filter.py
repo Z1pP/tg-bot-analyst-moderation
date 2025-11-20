48ddf18d1e269605086aac6f7171f6fb02589a57
Z1pP <maxm_99@mail.ru>
2025-11-20 16:07:08 +0300

    Enhance ChatTypeFilter to support both Message and CallbackQuery events, improving versatility in chat type validation.

--- src/filters/group_filter.py
+++ src/filters/group_filter.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 from aiogram.filters import Filter
-from aiogram.types import Message
+from aiogram.types import CallbackQuery, Message
 
 from constants.enums import ChatType
 
@@ -18,5 +18,9 @@ class ChatTypeFilter(Filter):
     def __init__(self, chat_type: Optional[list[ChatType]] = None):
         self.chat_type = chat_type or [ChatType.PRIVATE]
 
-    async def __call__(self, message: Message, *args, **kwds):
-        return message.chat.type in self.chat_type
+    async def __call__(self, event: Message | CallbackQuery, *args, **kwds):
+        if isinstance(event, Message):
+            return event.chat.type in self.chat_type
+        elif isinstance(event, CallbackQuery):
+            return event.message.chat.type in self.chat_type
+        return False
