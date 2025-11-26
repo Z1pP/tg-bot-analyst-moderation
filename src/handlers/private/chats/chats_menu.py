a48895dd889fcb0260ba0bd7c72d02edfe1aed8a
Z1pP <maxm_99@mail.ru>
2025-11-26 22:29:17 +0300

    Refactor chats_menu.py to update callback query handling and replace inline keyboard with chats_management_ikb for improved chat management experience.

--- src/handlers/private/chats/chats_menu.py
+++ src/handlers/private/chats/chats_menu.py
@@ -7,7 +7,7 @@ from constants import Dialog
 from constants.callback import CallbackData
 from constants.i18n import DEFAULT_LANGUAGE
 from container import container
-from keyboards.inline.chats_kb import chats_menu_ikb
+from keyboards.inline.chats_kb import chats_management_ikb
 from keyboards.inline.menu import admin_menu_ikb
 from services.user import UserService
 from states import MenuStates
@@ -19,7 +19,7 @@ router = Router(name=__name__)
 logger = logging.getLogger(__name__)
 
 
-@router.callback_query(F.data == CallbackData.Chat.CHATS_MENU)
+@router.callback_query(F.data == CallbackData.Chat.BACK_TO_CHATS_MANAGEMENT)
 async def show_chats_menu_handler(
     callback: types.CallbackQuery,
     state: FSMContext,
@@ -31,8 +31,8 @@ async def show_chats_menu_handler(
         bot=callback.bot,
         chat_id=callback.message.chat.id,
         message_id=callback.message.message_id,
-        text=Dialog.Chat.SELECT_ACTION,
-        reply_markup=chats_menu_ikb(),
+        text=Dialog.Chat.CHAT_MANAGEMENT,
+        reply_markup=chats_management_ikb(),
     )
 
     await log_and_set_state(
