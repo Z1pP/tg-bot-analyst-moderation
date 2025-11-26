eea6b21774bddee4413a55e42d5ac0918d4ec6f8
Z1pP <maxm_99@mail.ru>
2025-11-27 00:44:33 +0300

    Add add_chat_callback_handler to enable chat addition functionality via callback. This new handler logs user actions and provides instructions for adding chats, enhancing user interaction and chat management capabilities.

---
+++ src/handlers/private/chats/add.py
@@ -0,0 +1,30 @@
+import logging
+
+from aiogram import F, Router
+from aiogram.fsm.context import FSMContext
+from aiogram.types import CallbackQuery
+
+from constants import Dialog
+from constants.callback import CallbackData
+from keyboards.inline.chats import chats_management_ikb
+
+router = Router(name=__name__)
+logger = logging.getLogger(__name__)
+
+
+@router.callback_query(F.data == CallbackData.Chat.ADD)
+async def add_chat_callback_handler(callback: CallbackQuery, state: FSMContext) -> None:
+    """
+    Хендлер для команды добавления чата через callback.
+    """
+    await callback.answer()
+
+    logger.info(
+        "Пользователь %s начал добавление чата в отслеживание",
+        callback.from_user.username,
+    )
+
+    await callback.message.edit_text(
+        text=Dialog.Chat.ADD_CHAT_INSTRUCTION,
+        reply_markup=chats_management_ikb(),
+    )
