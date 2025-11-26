88be23ad280ecaa8772cf77b28a1fbef060b95fa
Z1pP <maxm_99@mail.ru>
2025-11-26 22:28:41 +0300

    Add archive router to chat handlers for improved chat management functionality.

--- src/handlers/private/chats/__init__.py
+++ src/handlers/private/chats/__init__.py
@@ -1,6 +1,7 @@
 from aiogram import Router
 
 from .add_chat import router as add_chat_router
+from .archive import router as archive_router
 from .chat_daily_rating import router as chat_daily_rating_router
 from .chats_menu import router as chats_menu_router
 from .delete_chat import router as delete_chat_router
@@ -14,3 +15,4 @@ router.include_router(chat_daily_rating_router)
 router.include_router(delete_chat_router)
 router.include_router(select_specific_chat_router)
 router.include_router(pagination_router)
+router.include_router(archive_router)
