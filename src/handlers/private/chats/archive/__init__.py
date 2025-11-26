e49470c66b6e823c9f6bd919deb9e60d576b23f1
Z1pP <maxm_99@mail.ru>
2025-11-27 00:46:24 +0300

    Remove archive channel setting handler and related router to enhance code maintainability by eliminating unused components associated with chat archiving functionality.

--- src/handlers/private/chats/archive/__init__.py
+++
@@ -1,7 +0,0 @@
-from aiogram import Router
-
-from .archive_channel_setting import router as archive_channel_setting_router
-
-router = Router(name=__name__)
-
-router.include_router(archive_channel_setting_router)
