2771480da5827772f9337ad311638d9cd93a2ffb
Z1pP <maxm_99@mail.ru>
2025-11-27 00:44:19 +0300

    Refactor chat router imports in __init__.py to improve organization and clarity. Renamed modules for better alignment with their functionality and updated included routers to reflect the new structure, enhancing maintainability and readability.

--- src/handlers/private/chats/__init__.py
+++ src/handlers/private/chats/__init__.py
@@ -1,18 +1,20 @@
 from aiogram import Router
 
-from .add_chat import router as add_chat_router
+from .add import router as add_chat_router
 from .archive import router as archive_router
-from .chat_daily_rating import router as chat_daily_rating_router
-from .chats_menu import router as chats_menu_router
-from .delete_chat import router as delete_chat_router
+from .dashboard import router as dashboard_router
+from .list import router as list_chats_router
+from .menu import router as chats_menu_router
 from .pagination import router as pagination_router
-from .select_specific_chat import router as select_specific_chat_router
+from .rating import router as chat_daily_rating_router
+from .untrack import router as untrack_chat_router
 
 router = Router(name="chats_router")
 router.include_router(add_chat_router)
 router.include_router(chats_menu_router)
 router.include_router(chat_daily_rating_router)
-router.include_router(delete_chat_router)
-router.include_router(select_specific_chat_router)
+router.include_router(untrack_chat_router)
+router.include_router(list_chats_router)
+router.include_router(dashboard_router)
 router.include_router(pagination_router)
 router.include_router(archive_router)
