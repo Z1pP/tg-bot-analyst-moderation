a7960e673f822a1baca3624a3741513370c17025
Z1pP <maxm_99@mail.ru>
2025-11-30 17:49:36 +0300

    Remove unused selecting_all_chats state from ChatStateManager to improve code clarity and maintainability.

--- src/states/chat_states.py
+++ src/states/chat_states.py
@@ -6,7 +6,6 @@ class ChatStateManager(StatesGroup):
     listing_tracking_chats = State()
 
     selecting_chat = State()
-    selecting_all_chats = State()
 
     selecting_period = State()
     selecting_custom_period = State()
