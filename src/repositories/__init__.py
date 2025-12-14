d8a7a5be8399ee5485dde1891f18818c998d3bed
Z1pP <maxm_99@mail.ru>
2025-12-14 17:30:28 +0300

    Add ReleaseNoteRepository and ReportScheduleRepository to repository initialization, enhancing modularity and access to new functionalities.

--- src/repositories/__init__.py
+++ src/repositories/__init__.py
@@ -8,6 +8,8 @@ from .message_repository import MessageRepository
 from .punishment_ladder_repository import PunishmentLadderRepository
 from .punishment_repository import PunishmentRepository
 from .reaction_repository import MessageReactionRepository
+from .release_note_repository import ReleaseNoteRepository
+from .report_schedule_repository import ReportScheduleRepository
 from .template_repository import MessageTemplateRepository
 from .user_chat_status_repository import UserChatStatusRepository
 from .user_repository import UserRepository
@@ -28,4 +30,6 @@ __all__ = [
     "PunishmentRepository",
     "PunishmentLadderRepository",
     "UserChatStatusRepository",
+    "ReportScheduleRepository",
+    "ReleaseNoteRepository",
 ]
