8e88c1ba57cf3a9e92a6621ae2f8c7c8f145164e
Z1pP <maxm_99@mail.ru>
2025-12-21 23:33:04 +0300

    Add IAIService abstract class for chat message summarization, defining the structure for summarization implementations.

---
+++ src/services/chat/summarize/ai_service_base.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+
+class IAIService(ABC):
+    """Absrtract class for summarize chat messages"""
+
+    def __init__(self, model_name: str) -> None:
+        self._model_name = model_name
+
+    @abstractmethod
+    async def summarize_text(self, text: str) -> str:
+        pass
