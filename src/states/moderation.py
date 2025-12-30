dfb5094d652bfa20bdf0b2e6c4cb981ef461b02e
Z1pP <maxm_99@mail.ru>
2025-12-30 23:42:19 +0300

    Add moderation state classes for user management, including moderation, amnesty, ban, and warning states to enhance state management in the application.

---
+++ src/states/moderation.py
@@ -0,0 +1,24 @@
+from aiogram.fsm.state import State, StatesGroup
+
+
+class ModerationStates(StatesGroup):
+    menu = State()
+
+
+class AmnestyStates(StatesGroup):
+    waiting_user_input = State()
+    waiting_chat_select = State()
+    waiting_confirmation_action = State()
+    waiting_action_select = State()
+
+
+class BanUserStates(StatesGroup):
+    waiting_user_input = State()
+    waiting_reason_input = State()
+    waiting_chat_select = State()
+
+
+class WarnUserStates(StatesGroup):
+    waiting_user_input = State()
+    waiting_reason_input = State()
+    waiting_chat_select = State()
