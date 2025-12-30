eac7e42f941d390704a1f9f26c3892fc7c4a4c29
Z1pP <maxm_99@mail.ru>
2025-12-31 01:39:12 +0300

    Update __init__.py to include new punishment mapper import and extend __all__ list for enhanced user moderation functionality.

--- src/mappers/__init__.py
+++ src/mappers/__init__.py
@@ -1,3 +1,4 @@
 from .moderation_mapper import map_message_to_moderation_dto
+from .punishment_mapper import map_temp_ladder_to_update_dto
 
-__all__ = ["map_message_to_moderation_dto"]
+__all__ = ["map_message_to_moderation_dto", "map_temp_ladder_to_update_dto"]
