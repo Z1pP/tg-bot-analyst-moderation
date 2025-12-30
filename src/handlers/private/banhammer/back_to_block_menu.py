1e6ad66245e3a84dad6232eea20c44623166923f
Z1pP <maxm_99@mail.ru>
2025-12-30 23:32:46 +0300

    Update back_to_block_menu handler to utilize moderation menu and state references, enhancing consistency in moderation functionality.

--- src/handlers/private/banhammer/back_to_block_menu.py
+++ src/handlers/private/banhammer/back_to_block_menu.py
@@ -2,8 +2,8 @@ from aiogram import F, Router, types
 from aiogram.fsm.context import FSMContext
 
 from constants import Dialog, InlineButtons
-from keyboards.inline.banhammer import block_actions_ikb
-from states.banhammer_states import BanHammerStates
+from keyboards.inline.banhammer import moderation_menu_ikb
+from states.banhammer_states import ModerationStates
 from utils.state_logger import log_and_set_state
 
 router = Router(name=__name__)
@@ -20,12 +20,12 @@ async def back_to_block_menu_handler(
     await callback.answer()
 
     await callback.message.edit_text(
-        text=Dialog.BlockMenu.SELECT_ACTION,
-        reply_markup=block_actions_ikb(),
+        text=Dialog.ModerationMenu.SELECT_ACTION,
+        reply_markup=moderation_menu_ikb(),
     )
 
     await log_and_set_state(
         message=callback.message,
         state=state,
-        new_state=BanHammerStates.block_menu,
+        new_state=ModerationStates.menu,
     )
