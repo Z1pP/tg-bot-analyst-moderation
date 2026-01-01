0567966f3e12bace69431f308af789d9556c9c59
Z1pP <maxm_99@mail.ru>
2026-01-01 20:41:26 +0300

    Refactor AmnestyUserDTO and CancelWarnResultDTO to inherit from BaseModel, enhancing data validation and serialization. Update field definitions to use Pydantic's Field for default values and add model configuration for immutability.

--- src/dto/amnesty.py
+++ src/dto/amnesty.py
@@ -1,25 +1,29 @@
-from dataclasses import dataclass, field
 from typing import List, Optional
 
+from pydantic import BaseModel, ConfigDict, Field
+
 from constants.punishment import PunishmentType
+
 from .chat_dto import ChatDTO
 
 
-@dataclass(frozen=True)
-class AmnestyUserDTO:
+class AmnestyUserDTO(BaseModel):
     violator_username: str
     violator_tgid: str
     violator_id: int
     admin_tgid: str
     admin_username: str
-    chat_dtos: List[ChatDTO] = field(default_factory=list)
+    chat_dtos: List[ChatDTO] = Field(default_factory=list)
+
+    model_config = ConfigDict(frozen=True)
 
 
-@dataclass(frozen=True, slots=True)
-class CancelWarnResultDTO:
+class CancelWarnResultDTO(BaseModel):
     """Результат отмены предупреждения"""
 
     success: bool
     current_warns_count: int
     next_punishment_type: Optional[PunishmentType]
     next_punishment_duration: Optional[int]
+
+    model_config = ConfigDict(frozen=True)
