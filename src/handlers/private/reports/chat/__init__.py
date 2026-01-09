6ba29f52f7edecd8df10993c8a1f0724e076d5a7
Z1pP <maxm_99@mail.ru>
2026-01-09 10:51:39 +0300

    Integrate chat_summary_handler into chat report router for enhanced reporting capabilities

--- src/handlers/private/reports/chat/__init__.py
+++ src/handlers/private/reports/chat/__init__.py
@@ -3,9 +3,11 @@ from aiogram import Router
 from .back_to_chat_actions_handler import router as back_to_chat_actions_router
 from .back_to_periods_handler import router as back_to_periods_router
 from .chat_report_handler import router as chat_report_router
+from .chat_summary_handler import router as chat_summary_router
 
 router = Router(name="chat_report_router")
 
 router.include_router(chat_report_router)
+router.include_router(chat_summary_router)
 router.include_router(back_to_periods_router)
 router.include_router(back_to_chat_actions_router)
