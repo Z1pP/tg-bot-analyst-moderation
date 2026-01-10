3ccad1f040daf599319c757b8a56207a16482801
Z1pP <maxm_99@mail.ru>
2026-01-10 14:52:28 +0300

    Add RestrictNewMemberUseCase and VerifyMemberUseCase to moderation use cases for enhanced member management functionality.

--- src/usecases/moderation/__init__.py
+++ src/usecases/moderation/__init__.py
@@ -1,7 +1,11 @@
 from .give_ban_user import GiveUserBanUseCase
 from .give_warn_user import GiveUserWarnUseCase
+from .restrict_new_member_use_case import RestrictNewMemberUseCase
+from .verify_member_use_case import VerifyMemberUseCase
 
 __all__ = [
     "GiveUserWarnUseCase",
     "GiveUserBanUseCase",
+    "RestrictNewMemberUseCase",
+    "VerifyMemberUseCase",
 ]
