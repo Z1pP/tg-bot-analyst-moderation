59131665775228085eba2e63760fe2638cbf6147
Z1pP <maxm_99@mail.ru>
2026-01-10 14:47:18 +0300

    Add ANTIBOT_TOGGLE and ANTIBOT_SETTING constants to ChatCallbackData for improved anti-bot functionality in chat moderation

--- src/constants/callback.py
+++ src/constants/callback.py
@@ -25,6 +25,8 @@ class ChatCallbackData:
     PUNISHMENT_SETTING = "punishment_setting"
     PUNISHMENT_CREATE_NEW = "punishment_create_new"
     PUNISHMENT_SET_DEFAULT = "punishment_set_default"
+    ANTIBOT_TOGGLE = "chat_antibot_toggle"
+    ANTIBOT_SETTING = "chat_antibot_setting"
     ALL_CHATS = "chat__all"
     CHATS_PAGE_INFO = "chats_page_info"
     REMOVE_CHATS_PAGE_INFO = "remove_chats_page_info"
