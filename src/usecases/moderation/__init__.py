34a521dbeb73ad59fc579d5fd8214f61dd341ae3
Z1pP <maxm_99@mail.ru>
2026-01-10 16:20:16 +0300

    Refactor moderation use cases by renaming import paths and adding RestrictNewMemberUseCase and VerifyMemberUseCase for enhanced member management and verification workflows.

--- src/usecases/moderation/__init__.py
+++ src/usecases/moderation/__init__.py
@@ -1,7 +1,7 @@
 from .give_ban_user import GiveUserBanUseCase
 from .give_warn_user import GiveUserWarnUseCase
-from .restrict_new_member_use_case import RestrictNewMemberUseCase
-from .verify_member_use_case import VerifyMemberUseCase
+from .restrict_new_member import RestrictNewMemberUseCase
+from .verify_member import VerifyMemberUseCase
 
 __all__ = [
     "GiveUserWarnUseCase",
