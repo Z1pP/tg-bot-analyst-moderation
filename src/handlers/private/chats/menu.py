0b32e64b2e6f657b1407f6df3be1988c953c016e
Z1pP <maxm_99@mail.ru>
2026-01-11 02:05:06 +0300

    Refactor chat menu handlers to utilize common navigation functions for improved code organization and maintainability. This change enhances the clarity of state management and reduces redundancy in message editing logic.

--- src/handlers/private/chats/menu.py
+++ src/handlers/private/chats/menu.py
@@ -3,16 +3,9 @@ import logging
 from aiogram import F, Router, types
 from aiogram.fsm.context import FSMContext
 
-from constants import Dialog
 from constants.callback import CallbackData
-from constants.i18n import DEFAULT_LANGUAGE
-from container import container
-from keyboards.inline.chats import chats_management_ikb
-from keyboards.inline.menu import admin_menu_ikb
-from services.user import UserService
-from states import MenuStates
-from states.chat import ChatStateManager
-from utils.send_message import safe_edit_message
+
+from ..common.navigation import show_chats_menu, show_main_menu
 
 router = Router(name=__name__)
 logger = logging.getLogger(__name__)
@@ -24,47 +17,15 @@ async def show_chats_menu_handler(
     state: FSMContext,
 ) -> None:
     await callback.answer()
-    await state.clear()
-
-    await safe_edit_message(
-        bot=callback.bot,
-        chat_id=callback.message.chat.id,
-        message_id=callback.message.message_id,
-        text=Dialog.Chat.CHAT_MANAGEMENT,
-        reply_markup=chats_management_ikb(),
-    )
-
-    await state.set_state(ChatStateManager.chats_menu)
+    await show_chats_menu(callback, state)
 
 
 @router.callback_query(F.data == CallbackData.Chat.BACK_TO_MAIN_MENU_FROM_CHATS)
 async def return_to_main_menu_handler(
     callback: types.CallbackQuery,
     state: FSMContext,
+    user_language: str,
 ) -> None:
     """Обработчик возврата в главное меню из меню чатов"""
     await callback.answer()
-    await state.clear()
-
-    username = callback.from_user.first_name
-    menu_text = Dialog.Menu.MENU_TEXT.format(username=username)
-
-    # Получаем язык пользователя из БД
-    user_service: UserService = container.resolve(UserService)
-    db_user = await user_service.get_user(tg_id=str(callback.from_user.id))
-    user_language = (
-        db_user.language if db_user and db_user.language else DEFAULT_LANGUAGE
-    )
-
-    await safe_edit_message(
-        bot=callback.bot,
-        chat_id=callback.message.chat.id,
-        message_id=callback.message.message_id,
-        text=menu_text,
-        reply_markup=admin_menu_ikb(
-            user_language=user_language,
-            admin_tg_id=str(callback.from_user.id),
-        ),
-    )
-
-    await state.set_state(MenuStates.main_menu)
+    await show_main_menu(callback, state, user_language)
