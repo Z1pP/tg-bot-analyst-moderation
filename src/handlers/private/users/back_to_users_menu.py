cc0032132edb35571442c2cc4ccad58d2074b8d5
Z1pP <maxm_99@mail.ru>
2026-01-18 15:44:58 +0300

    Refactor back_to_users_menu_handler to use updated callback data and integrate HasTrackedUsersUseCase for dynamic reply markup based on tracked users status.

--- src/handlers/private/users/back_to_users_menu.py
+++ src/handlers/private/users/back_to_users_menu.py
@@ -1,20 +1,27 @@
 from aiogram import F, Router, types
 from aiogram.fsm.context import FSMContext
+from punq import Container
 
 from constants.callback import CallbackData
 from keyboards.inline.users import users_menu_ikb
 from states import UserStateManager
+from usecases.user_tracking import HasTrackedUsersUseCase
 
 router = Router(name=__name__)
 
 
-@router.callback_query(F.data == CallbackData.User.USERS_MENU)
-async def back_to_users_menu_handler(callback: types.CallbackQuery, state: FSMContext):
+@router.callback_query(F.data == CallbackData.User.MENU)
+async def back_to_users_menu_handler(
+    callback: types.CallbackQuery, state: FSMContext, container: Container
+):
     await callback.answer()
+
+    usecase: HasTrackedUsersUseCase = container.resolve(HasTrackedUsersUseCase)
+    has_tracked_users = await usecase.execute(admin_tgid=str(callback.from_user.id))
 
     await callback.message.edit_text(
         text="Выбери действие:",
-        reply_markup=users_menu_ikb(),
+        reply_markup=users_menu_ikb(has_tracked_users=has_tracked_users),
     )
 
     await state.set_state(UserStateManager.users_menu)
