e40ca9bc3ddf0f61cc90c584862fe49297a414ee
Z1pP <maxm_99@mail.ru>
2026-01-18 16:47:42 +0300

    Update UsernameStates to include waiting_add_user_data_input and waiting_remove_user_data_input for enhanced user data management.

--- src/states/user.py
+++ src/states/user.py
@@ -4,7 +4,8 @@ from aiogram.fsm.state import State, StatesGroup
 class UsernameStates(StatesGroup):
     """Состояние для задавания имени пользователя"""
 
-    waiting_user_data_input = State()
+    waiting_add_user_data_input = State()
+    waiting_remove_user_data_input = State()
 
 
 class SingleUserReportStates(StatesGroup):
