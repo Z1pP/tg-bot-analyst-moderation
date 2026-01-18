e1250441211877203d93c4faeeb30c451a4633a6
Z1pP <maxm_99@mail.ru>
2026-01-18 14:50:25 +0300

    Update users router to include menu handler, enhancing user management navigation.

--- src/handlers/private/users/__init__.py
+++ src/handlers/private/users/__init__.py
@@ -4,10 +4,10 @@ from .add_user_to_tracking import router as add_router
 from .back_to_users_menu import router as back_router
 from .delete_user_from_tracking import router as delete_router
 from .list_tracked_users import router as list_router
+from .menu import router as menu_router
 from .pagination import router as pagination_router
 from .select_all_users import router as select_all_router
 from .select_specific_user import router as select_specific_router
-from .users_menu import router as menu_router
 
 router = Router(name="users_router")
 router.include_router(menu_router)
