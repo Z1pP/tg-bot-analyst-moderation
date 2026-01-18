3f65e9a3f73990f776eef5b0acde4dc739775ce3
Z1pP <maxm_99@mail.ru>
2026-01-18 13:30:37 +0300

    Refactor GetUserByTgIdUseCase and GetUserByIdUseCase to utilize UserService instead of UserRepository for user retrieval. This change enhances code modularity and aligns with recent updates in user management.

--- src/usecases/user/get_user.py
+++ src/usecases/user/get_user.py
@@ -1,20 +1,20 @@
 from typing import Optional
 
 from models import User
-from repositories import UserRepository
+from services import UserService
 
 
 class GetUserByTgIdUseCase:
-    def __init__(self, user_repository: UserRepository):
-        self.user_repository = user_repository
+    def __init__(self, user_service: UserService):
+        self._user_service = user_service
 
     async def execute(self, tg_id: str) -> Optional[User]:
-        return await self.user_repository.get_user_by_tg_id(tg_id=tg_id)
+        return await self._user_service.get_user(tg_id=tg_id)
 
 
 class GetUserByIdUseCase:
-    def __init__(self, user_repository: UserRepository):
-        self.user_repository = user_repository
+    def __init__(self, user_service: UserService):
+        self._user_service = user_service
 
     async def execute(self, user_id: int) -> Optional[User]:
-        return await self.user_repository.get_user_by_id(user_id)
+        return await self._user_service.get_user_by_id(user_id)
