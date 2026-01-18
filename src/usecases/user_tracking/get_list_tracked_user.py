1c0b41b68f3f0a8226e77370c6420c6d7a627c01
Z1pP <maxm_99@mail.ru>
2026-01-18 15:46:15 +0300

    Refactor GetListTrackedUsersUseCase to utilize UserDTO for improved data handling and update user retrieval method to include addition dates.

--- src/usecases/user_tracking/get_list_tracked_user.py
+++ src/usecases/user_tracking/get_list_tracked_user.py
@@ -1,4 +1,4 @@
-from models import User
+from dto.user import UserDTO
 from repositories import UserTrackingRepository
 
 
@@ -6,7 +6,13 @@ class GetListTrackedUsersUseCase:
     def __init__(self, user_tracking_repository: UserTrackingRepository):
         self._user_tracking_repository = user_tracking_repository
 
-    async def execute(self, admin_tgid: str) -> list[User]:
-        return await self._user_tracking_repository.get_tracked_users_by_admin(
-            admin_tgid=admin_tgid
+    async def execute(self, admin_tgid: str) -> list[UserDTO]:
+        users_with_dates = (
+            await self._user_tracking_repository.get_tracked_users_with_dates(
+                admin_tgid=admin_tgid
+            )
         )
+        return [
+            UserDTO.from_model(user, added_at=added_at)
+            for user, added_at in users_with_dates
+        ]
