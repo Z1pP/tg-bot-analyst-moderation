c15416ddffc2efaf401210db388eb643bc3d2a66
Z1pP <maxm_99@mail.ru>
2026-01-18 12:44:11 +0300

    Enhance SaveMessageUseCase to integrate UserService and ChatService, allowing for user and chat entity creation or retrieval based on Telegram IDs, and update BufferedMessageDTO construction accordingly.

--- src/usecases/message/save_message.py
+++ src/usecases/message/save_message.py
@@ -1,11 +1,20 @@
 from dto.buffer import BufferedMessageDTO
 from dto.message import CreateMessageDTO
 from services.analytics_buffer_service import AnalyticsBufferService
+from services.chat.chat_service import ChatService
+from services.user.user_service import UserService
 
 
 class SaveMessageUseCase:
-    def __init__(self, buffer_service: AnalyticsBufferService):
+    def __init__(
+        self,
+        buffer_service: AnalyticsBufferService,
+        user_service: UserService,
+        chat_service: ChatService,
+    ):
         self.buffer_service = buffer_service
+        self.user_service = user_service
+        self.chat_service = chat_service
 
     async def execute(self, message_dto: CreateMessageDTO) -> None:
         """
@@ -14,10 +23,18 @@ class SaveMessageUseCase:
         Возвращает "заглушку" ResultMessageDTO, так как сообщение еще не сохранено в БД.
         ID будет присвоен после обработки воркером.
         """
+
+        user = await self.user_service.get_or_create(
+            tg_id=message_dto.user_tgid,
+        )
+        chat = await self.chat_service.get_or_create(
+            chat_tgid=message_dto.chat_tgid,
+        )
+
         # Конвертируем CreateMessageDTO в BufferedMessageDTO
         buffered_dto = BufferedMessageDTO(
-            chat_id=message_dto.chat_id,
-            user_id=message_dto.user_id,
+            chat_id=chat.id,
+            user_id=user.id,
             message_id=message_dto.message_id,
             message_type=message_dto.message_type,
             content_type=message_dto.content_type,
