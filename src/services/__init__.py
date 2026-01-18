e0382d63b3a7d434883150c1aac48c113c704b2a
Z1pP <maxm_99@mail.ru>
2026-01-18 13:24:28 +0300

    Update service imports in __init__.py to include new services: AnalyticsBufferService, BreakAnalysisService, CategoryService, ReleaseNoteService, TemplateContentService, TemplateService, and TaskiqSchedulerService, enhancing modularity and accessibility of service classes.

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,18 +1,32 @@
 from .admin_action_log_service import AdminActionLogService
+from .analytics_buffer_service import AnalyticsBufferService
+from .break_analysis_service import BreakAnalysisService
+from .categories.category_service import CategoryService
 from .chat import ArchiveBindService, ChatService
 from .messaging import BotMessageService
 from .permissions import BotPermissionService
 from .punishment_service import PunishmentService
+from .release_note_service import ReleaseNoteService
 from .report_schedule_service import ReportScheduleService
+from .scheduler.taskiq_scheduler import TaskiqSchedulerService
+from .templates.content_service import TemplateContentService
+from .templates.template_service import TemplateService
 from .user import UserService
 
 __all__ = [
     "AdminActionLogService",
+    "AnalyticsBufferService",
     "ArchiveBindService",
-    "ChatService",
+    "BreakAnalysisService",
     "BotMessageService",
     "UserService",
     "PunishmentService",
     "BotPermissionService",
     "ReportScheduleService",
+    "CategoryService",
+    "TemplateService",
+    "TemplateContentService",
+    "ReleaseNoteService",
+    "TaskiqSchedulerService",
+    "ChatService",
 ]
