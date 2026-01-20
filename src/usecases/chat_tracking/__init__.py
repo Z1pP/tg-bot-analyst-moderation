366cd626a2c096fb48d96c77a8b5756664a7ed59
Z1pP <maxm_99@mail.ru>
2026-01-20 11:29:03 +0300

    Update __init__.py to include AddChatToTrackResult in the exported symbols, enhancing the chat tracking use case functionality.

--- src/usecases/chat_tracking/__init__.py
+++ src/usecases/chat_tracking/__init__.py
@@ -1,9 +1,10 @@
-from .add_chat_to_track_usecase import AddChatToTrackUseCase
+from .add_chat_to_track_usecase import AddChatToTrackResult, AddChatToTrackUseCase
 from .get_user_tracked_chats import GetUserTrackedChatsUseCase
 from .remove_chat_from_tracking import RemoveChatFromTrackingUseCase
 
 __all__ = [
     "AddChatToTrackUseCase",
+    "AddChatToTrackResult",
     "GetUserTrackedChatsUseCase",
     "RemoveChatFromTrackingUseCase",
 ]
