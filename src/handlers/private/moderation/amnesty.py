4b003a45388b5e45075af78f74ac0ff2498f3647
Z1pP <maxm_99@mail.ru>
2026-01-20 11:27:41 +0300

    Update invalid username format dialog message in amnesty.py to enhance user feedback during moderation actions.

--- src/handlers/private/moderation/amnesty.py
+++ src/handlers/private/moderation/amnesty.py
@@ -66,7 +66,7 @@ async def waiting_user_data_input(
         bot=bot,
         container=container,
         dialog_texts={
-            "invalid_format": Dialog.User.INVALID_USERNAME_FORMAT,
+            "invalid_format": Dialog.User.INVALID_USERNAME_FORMAT_ADD,
             "user_not_found": Dialog.BanUser.USER_NOT_FOUND,
             "user_info": Dialog.AmnestyUser.SELECT_ACTION,
         },
