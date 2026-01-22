b1c1356de9783fbf6ba88515a74c15fc728d21a3
Z1pP <maxm_99@mail.ru>
2026-01-22 21:42:47 +0300

    Add archive usecases package with initial implementation of GetArchiveSettingsUseCase and ArchiveSettingsResult for managing archive settings.

---
+++ src/usecases/archive/__init__.py
@@ -0,0 +1,8 @@
+"""Archive usecases package."""
+
+from .get_archive_settings import ArchiveSettingsResult, GetArchiveSettingsUseCase
+
+__all__ = [
+    "ArchiveSettingsResult",
+    "GetArchiveSettingsUseCase",
+]
