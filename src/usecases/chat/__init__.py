a1c672df3b9b5658b43447471caa5accde936e28
Z1pP <maxm_99@mail.ru>
2026-01-22 23:54:30 +0300

    Refactor import statements in chat use cases to include ToggleAutoDeleteWelcomeText and ToggleWelcomeText. This improves code organization and enhances the clarity of available chat management functionalities.

--- src/usecases/chat/__init__.py
+++ src/usecases/chat/__init__.py
@@ -1,7 +1,11 @@
 from .get_all_chats import GetAllChatsUseCase
 from .get_chats_for_user_action import GetChatsForUserActionUseCase
 from .get_tracked_chats import GetTrackedChatsUseCase
-from .toggle_antibot_use_case import ToggleAntibotUseCase
+from .toggle_antibot import ToggleAntibotUseCase
+from .toggle_auto_delete_welcome_text import (
+    ToggleAutoDeleteWelcomeTextUseCase,
+)
+from .toggle_welcome_text import ToggleWelcomeTextUseCase
 from .update_chat_welcome_text import UpdateChatWelcomeTextUseCase
 from .update_chat_work_hours import UpdateChatWorkHoursUseCase
 
@@ -11,5 +15,7 @@ __all__ = [
     "GetChatsForUserActionUseCase",
     "UpdateChatWorkHoursUseCase",
     "ToggleAntibotUseCase",
+    "ToggleAutoDeleteWelcomeTextUseCase",
+    "ToggleWelcomeTextUseCase",
     "UpdateChatWelcomeTextUseCase",
 ]
