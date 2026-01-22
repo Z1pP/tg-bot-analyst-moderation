92c4b38dac6af931183ac9fce1ecfa42ba8787d9
Z1pP <maxm_99@mail.ru>
2026-01-22 22:44:35 +0300

    Add welcome text menu handler to manage welcome text settings, improving user interaction and chat management functionality.

---
+++ src/handlers/private/chats/welcome_text/settings.py
@@ -0,0 +1,74 @@
+import logging
+
+from aiogram import F, Router
+from aiogram.fsm.context import FSMContext
+from aiogram.types import CallbackQuery
+from punq import Container
+
+from constants import Dialog
+from constants.callback import CallbackData
+from keyboards.inline.chats import chats_management_ikb, welcome_text_setting_ikb
+from services.chat import ChatService
+from utils.send_message import safe_edit_message
+
+router = Router(name=__name__)
+logger = logging.getLogger(__name__)
+
+
+@router.callback_query(F.data == CallbackData.Chat.WELCOME_TEXT_SETTING)
+async def welcome_text_menu_handler(
+    callback: CallbackQuery,
+    state: FSMContext,
+    container: Container,
+) -> None:
+    """Обработчик нажатия на кнопку настройки приветственного текста"""
+    await callback.answer()
+
+    chat_id = await state.get_value("chat_id")
+    if not chat_id:
+        logger.error("chat_id не найден в state")
+        await safe_edit_message(
+            bot=callback.bot,
+            chat_id=callback.message.chat.id,
+            message_id=callback.message.message_id,
+            text=Dialog.Chat.CHAT_NOT_SELECTED,
+            reply_markup=chats_management_ikb(),
+        )
+        return
+
+    chat_service: ChatService = container.resolve(ChatService)
+    chat = await chat_service.get_chat_with_archive(chat_id=chat_id)
+
+    if chat is None:
+        await safe_edit_message(
+            bot=callback.bot,
+            chat_id=callback.message.chat.id,
+            message_id=callback.message.message_id,
+            text=Dialog.Chat.CHAT_NOT_FOUND_OR_ALREADY_REMOVED,
+            reply_markup=chats_management_ikb(),
+        )
+        return
+
+    antibot_status = "🟢 Включён" if chat.is_antibot_enabled else "🔴 Выключен"
+    welcome_text_status = "🟢 Включён" if chat.welcome_text else "🔴 Выключен"
+    # auto_delete_status = (
+    #     "🟢 Включён" if chat.auto_delete_welcome_text else "🔴 Выключен"
+    # )
+
+    auto_delete_status = "🟢 Включён"
+
+    await safe_edit_message(
+        bot=callback.bot,
+        chat_id=callback.message.chat.id,
+        message_id=callback.message.message_id,
+        text=Dialog.Chat.ENTER_WELCOME_TEXT.format(
+            chat_title=chat.title,
+            antibot_status=antibot_status,
+            welcome_text_status=welcome_text_status,
+            auto_delete_status=auto_delete_status,
+        ),
+        reply_markup=welcome_text_setting_ikb(
+            welcome_text_enabled=True if chat.welcome_text else False,
+            auto_delete_enabled=True if auto_delete_status else False,
+        ),
+    )
