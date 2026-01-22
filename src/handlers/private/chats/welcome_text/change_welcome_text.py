0397750c463edf718f65df4925c34cc1808077e0
Z1pP <maxm_99@mail.ru>
2026-01-22 23:37:02 +0300

    Add handler for changing welcome text and improve error messaging in welcome text input process. Update reply markup for better user interaction.

--- src/handlers/private/chats/welcome_text/change_welcome_text.py
+++ src/handlers/private/chats/welcome_text/change_welcome_text.py
@@ -8,10 +8,10 @@ from punq import Container
 from constants import Dialog
 from constants.callback import CallbackData
 from keyboards.inline.chats import (
-    chat_actions_ikb,
+    cancel_welcome_text_setting_ikb,
+    hide_notification_ikb,
 )
-from services.chat import ChatService
-from states import ChatStateManager, WelcomeTextState
+from states import WelcomeTextState
 from usecases.chat import UpdateChatWelcomeTextUseCase
 from utils.send_message import safe_edit_message
 
@@ -19,18 +19,42 @@ router = Router(name=__name__)
 logger = logging.getLogger(__name__)
 
 
+@router.callback_query(F.data == CallbackData.Chat.CHANGE_WELCOME_TEXT)
+async def change_welcome_text_handler(
+    callback: CallbackQuery,
+    state: FSMContext,
+) -> None:
+    """Обработчик нажатия на кнопку изменения приветственного текста"""
+    await callback.answer()
+    await state.set_state(WelcomeTextState.waiting_welcome_text_input)
+
+    await safe_edit_message(
+        bot=callback.bot,
+        chat_id=callback.message.chat.id,
+        message_id=callback.message.message_id,
+        text=Dialog.Chat.ENTER_WELCOME_TEXT,
+        reply_markup=cancel_welcome_text_setting_ikb(),
+    )
+
+
 @router.message(WelcomeTextState.waiting_welcome_text_input)
 async def welcome_text_input_handler(
-    message: Message, state: FSMContext, container: Container
+    message: Message,
+    state: FSMContext,
+    container: Container,
 ) -> None:
     """Обработчик ввода нового приветственного текста"""
     data = await state.get_data()
-    active_message_id = data.get("active_message_id")
     chat_id = data.get("chat_id")
 
     if not chat_id:
-        logger.error("chat_id не найден в state")
-        await message.delete()
+        await safe_edit_message(
+            bot=message.bot,
+            chat_id=message.chat.id,
+            message_id=message.message_id,
+            text=Dialog.Chat.CHAT_NOT_SELECTED,
+            reply_markup=cancel_welcome_text_setting_ikb(),
+        )
         return
 
     try:
@@ -44,78 +68,27 @@ async def welcome_text_input_handler(
         )
 
         if not updated_chat:
-            text = "❌ Чат не найден. Попробуйте позже."
-            if active_message_id:
-                await safe_edit_message(
-                    bot=message.bot,
-                    chat_id=message.chat.id,
-                    message_id=active_message_id,
-                    text=text,
-                    reply_markup=chat_actions_ikb(),
-                )
+            await safe_edit_message(
+                bot=message.bot,
+                chat_id=message.chat.id,
+                message_id=message.message_id,
+                text=Dialog.Chat.CHAT_NOT_FOUND_OR_ALREADY_REMOVED,
+                reply_markup=cancel_welcome_text_setting_ikb(),
+            )
             return
 
-        await safe_edit_message(
-            bot=message.bot,
-            chat_id=message.chat.id,
-            message_id=active_message_id,
+        await message.answer(
             text=Dialog.Chat.WELCOME_TEXT_UPDATED,
-            reply_markup=chat_actions_ikb(),
+            reply_markup=hide_notification_ikb(),
         )
+
     except Exception as e:
         logger.error(
             "Ошибка при сохранении приветственного текста: %s", e, exc_info=True
         )
-        text = "❌ Произошла ошибка при сохранении текста. Попробуйте позже."
-        if active_message_id:
-            await safe_edit_message(
-                bot=message.bot,
-                chat_id=message.chat.id,
-                message_id=active_message_id,
-                text=text,
-                reply_markup=chat_actions_ikb(),
-            )
+        await message.answer(
+            text=Dialog.Chat.ERROR_SAVE_WELCOME_TEXT,
+            reply_markup=hide_notification_ikb(),
+        )
     finally:
         await message.delete()
-
-
-@router.callback_query(
-    F.data == CallbackData.Chat.BACK_TO_CHAT_ACTIONS,
-    WelcomeTextState.waiting_welcome_text_input,
-)
-async def back_to_chat_actions_handler(
-    callback: CallbackQuery,
-    state: FSMContext,
-    container: Container,
-) -> None:
-    """Обработчик возврата к меню чатов из настройки приветственного текста."""
-    await callback.answer()
-
-    chat_id = await state.get_value("chat_id")
-
-    chat_service: ChatService = container.resolve(ChatService)
-    chat = await chat_service.get_chat_with_archive(chat_id=chat_id)
-
-    if not chat:
-        await safe_edit_message(
-            bot=callback.bot,
-            chat_id=callback.message.chat.id,
-            message_id=callback.message.message_id,
-            text=Dialog.Chat.CHAT_NOT_FOUND_OR_ALREADY_REMOVED,
-            reply_markup=chat_actions_ikb(),
-        )
-        return
-
-    await safe_edit_message(
-        bot=callback.bot,
-        chat_id=callback.message.chat.id,
-        message_id=callback.message.message_id,
-        text=Dialog.Chat.CHAT_ACTIONS_INFO.format(
-            title=chat.title,
-            start_time=chat.start_time.strftime("%H:%M"),
-            end_time=chat.end_time.strftime("%H:%M"),
-        ),
-        reply_markup=chat_actions_ikb(),
-    )
-
-    await state.set_state(ChatStateManager.selecting_chat)
