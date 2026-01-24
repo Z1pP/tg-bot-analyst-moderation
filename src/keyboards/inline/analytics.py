75d8515b528f4253b9e245e3f7a9f272f0445cc4
Z1pP <maxm_99@mail.ru>
2026-01-24 21:38:27 +0300

    Add analytics_menu_ikb function to create an inline keyboard for user and chat tracking options

---
+++ src/keyboards/inline/analytics.py
@@ -0,0 +1,26 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+from constants import Dialog
+from constants.callback import CallbackData
+
+
+def analytics_menu_ikb() -> InlineKeyboardMarkup:
+    builder = InlineKeyboardBuilder()
+    builder.row(
+        InlineKeyboardButton(
+            text=Dialog.User.SHOW_TRACKED_USERS,
+            callback_data=CallbackData.User.SELECT_USER,
+        ),
+        InlineKeyboardButton(
+            text=Dialog.Chat.SHOW_TRACKED_CHATS,
+            callback_data=CallbackData.Chat.SELECT_CHAT,
+        ),
+        InlineKeyboardButton(
+            text=Dialog.Common.COME_BACK,
+            callback_data=CallbackData.Menu.SHOW_MENU,
+        ),
+    )
+
+    builder.adjust(2, 1)
+    return builder.as_markup()
