151efed4e0629de5082080cf5c0be373c8353d58
Z1pP <maxm_99@mail.ru>
2026-01-24 21:29:35 +0300

    Integrate analytics router into private handler and remove unused chats and users routers for cleaner structure.

--- src/handlers/private/__init__.py
+++ src/handlers/private/__init__.py
@@ -6,8 +6,8 @@ from .admin_logs import (
 from .admin_logs import (
     router as admin_logs_router,
 )
+from .analytics import router as analytics_router
 from .categories import router as categories_router
-from .chats import router as chats_router
 from .common import router as common_router
 from .message_management import router as message_management_router
 from .moderation import router as moderation_router
@@ -16,13 +16,11 @@ from .reports import router as reports_router
 from .roles import router as roles_router
 from .scheduler import router as scheduler_router
 from .templates import router as templates_router
-from .users import router as users_router
 
 router = Router(name="private_router")
 router.include_router(common_router)
+router.include_router(analytics_router)
 router.include_router(moderation_router)
-router.include_router(users_router)
-router.include_router(chats_router)
 router.include_router(reports_router)
 router.include_router(templates_router)
 router.include_router(categories_router)
