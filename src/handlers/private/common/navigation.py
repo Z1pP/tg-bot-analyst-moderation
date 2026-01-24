cda3b8da77eda436f014a1cd54cfef4abf1eec81
Z1pP <maxm_99@mail.ru>
2026-01-24 19:35:14 +0300

    Refactor show_main_menu to include user retrieval with error handling and update menu text dynamically based on user data.

--- src/handlers/private/common/navigation.py
+++ src/handlers/private/common/navigation.py
@@ -1,28 +1,49 @@
+import logging
+
 from aiogram.fsm.context import FSMContext
 from aiogram.types import CallbackQuery
+from punq import Container
 
 from constants import Dialog
 from keyboards.inline.chats import chats_management_ikb
 from keyboards.inline.menu import admin_menu_ikb
+from keyboards.inline.users import hide_notification_ikb
+from services.user import UserService
 from states import MenuStates
 from utils.send_message import safe_edit_message
 
+logger = logging.getLogger(__name__)
+
 
 async def show_main_menu(
-    callback: CallbackQuery, state: FSMContext, user_language: str
+    callback: CallbackQuery,
+    state: FSMContext,
+    user_language: str,
+    container: Container,
 ) -> None:
     """Helper to show the main menu and clear state."""
     await state.clear()
 
-    username = callback.from_user.first_name
-    menu_text = Dialog.Menu.MENU_TEXT.format(username=username)
+    try:
+        user_service: UserService = container.resolve(UserService)
+        user = await user_service.get_user(tg_id=str(callback.from_user.id))
+    except Exception as e:
+        logger.error("Error getting user: %s", e)
+        await safe_edit_message(
+            bot=callback.bot,
+            chat_id=callback.message.chat.id,
+            message_id=callback.message.message_id,
+            text=Dialog.User.ERROR_GET_USER,
+            reply_markup=hide_notification_ikb(),
+        )
 
     await safe_edit_message(
         bot=callback.bot,
         chat_id=callback.message.chat.id,
         message_id=callback.message.message_id,
-        text=menu_text,
+        text=Dialog.Menu.MENU_TEXT.format(username=user.username),
         reply_markup=admin_menu_ikb(
+            user=user,
             user_language=user_language,
             admin_tg_id=str(callback.from_user.id),
         ),
