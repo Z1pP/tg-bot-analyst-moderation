4dad4c8c6f2aedfa439e419c0e4bdf677b007f3b
Z1pP <maxm_99@mail.ru>
2026-01-26 00:31:41 +0300

    Refactor message management routing to include menu router, improving organization and user navigation within the message management module.

--- src/handlers/private/message_management/__init__.py
+++ src/handlers/private/message_management/__init__.py
@@ -2,14 +2,14 @@ from aiogram import Router
 
 from .action_select_handler import router as action_select_router
 from .delete_handler import router as delete_router
+from .menu import router as menu_router
 from .message_link_handler import router as message_link_router
 from .reply_handler import router as reply_router
 from .send_message_handler import router as send_message_router
-from .message_manager import router as message_management_router
 
 router = Router()
 
-router.include_router(message_management_router)
+router.include_router(menu_router)
 router.include_router(message_link_router)
 router.include_router(action_select_router)
 router.include_router(delete_router)
