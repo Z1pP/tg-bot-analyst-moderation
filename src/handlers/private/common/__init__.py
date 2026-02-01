21d489e565a67adbeb47ba0fec425d4687df71af
Z1pP <maxm_99@mail.ru>
2026-02-02 00:34:58 +0300

    Include users_chats_settings router in common router to enhance user and chat settings management functionality.

--- src/handlers/private/common/__init__.py
+++ src/handlers/private/common/__init__.py
@@ -2,7 +2,9 @@ from aiogram import Router
 
 from .menu_handler import router as menu_router
 from .start_handler import router as start_router
+from .users_chats_settings import router as users_chats_settings_router
 
 router = Router(name="common_router")
 router.include_router(menu_router)
 router.include_router(start_router)
+router.include_router(users_chats_settings_router)
