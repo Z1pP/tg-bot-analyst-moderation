b70342f90d826351d5a2b84967860267a36c5d51
Z1pP <maxm_99@mail.ru>
2026-02-02 00:35:05 +0300

    Refactor users_chats_settings_handler to simplify parameters and enhance message editing functionality
    This update modifies the users_chats_settings_handler by removing the container parameter and integrating the safe_edit_message utility for improved message handling in the user and chat settings menu.

--- src/handlers/private/common/users_chats_settings.py
+++ src/handlers/private/common/users_chats_settings.py
@@ -1,16 +1,23 @@
 from aiogram import F, Router
 from aiogram.types import CallbackQuery
-from punq import Container
 
+from constants import Dialog
 from constants.callback import CallbackData
+from keyboards.inline.users_chats_settings import users_chats_settings_ikb
+from utils.send_message import safe_edit_message
 
 router = Router(name=__name__)
 
 
 @router.callback_query(F.data == CallbackData.UserAndChatsSettings.SHOW_MENU)
-async def users_chats_settings_handler(
-    callback: CallbackQuery,
-    container: Container,
-):
+async def users_chats_settings_handler(callback: CallbackQuery) -> None:
     """Обработчик нажатия на кнопку настроек пользователей и чатов"""
     await callback.answer()
+
+    await safe_edit_message(
+        bot=callback.bot,
+        chat_id=callback.message.chat.id,
+        message_id=callback.message.message_id,
+        text=Dialog.UserAndChatsSettings.MENU_TEXT,
+        reply_markup=users_chats_settings_ikb(),
+    )
