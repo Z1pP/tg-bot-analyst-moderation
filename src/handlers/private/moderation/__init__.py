600eb90bbddac83d1ac3008d6033340e1eb6e8fe
Z1pP <maxm_99@mail.ru>
2026-02-05 14:10:48 +0300

    Update moderation handler to replace menu router with navigation router, enhancing code organization and clarity.

--- src/handlers/private/moderation/__init__.py
+++ src/handlers/private/moderation/__init__.py
@@ -2,12 +2,12 @@ from aiogram import Router
 
 from .amnesty import router as amnesty_router
 from .block import router as block_user_router
-from .menu import router as menu_router
+from .navigation import router as navigation_router
 from .warn import router as warn_user_router
 
 router = Router(name="moderation_router")
 
-router.include_router(menu_router)
+router.include_router(navigation_router)
 router.include_router(amnesty_router)
 router.include_router(block_user_router)
 router.include_router(warn_user_router)
