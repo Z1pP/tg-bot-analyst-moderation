bc6ca7776127cce712374dd16df2056effd8acad
Z1pP <maxm_99@mail.ru>
2026-02-05 13:39:23 +0300

    Refactor moderation handler imports to improve clarity by renaming block_user and warn_user modules for better organization.

--- src/handlers/private/moderation/__init__.py
+++ src/handlers/private/moderation/__init__.py
@@ -1,9 +1,9 @@
 from aiogram import Router
 
 from .amnesty import router as amnesty_router
-from .block_user import router as block_user_router
+from .block import router as block_user_router
 from .menu import router as menu_router
-from .warn_user import router as warn_user_router
+from .warn import router as warn_user_router
 
 router = Router(name="moderation_router")
 
