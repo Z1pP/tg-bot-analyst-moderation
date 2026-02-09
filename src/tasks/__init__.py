73abcfb0b0a491257332b9b02795cf46fc8d94c9
Z1pP <maxm_99@mail.ru>
2026-02-09 19:09:26 +0300

    Add delete_welcome_message_task to task exports for moderation functionality

--- src/tasks/__init__.py
+++ src/tasks/__init__.py
@@ -3,6 +3,7 @@ from .analytics_tasks import (
     process_buffered_reactions_task,
     process_buffered_replies_task,
 )
+from .moderation_tasks import delete_welcome_message_task
 from .report_tasks import send_chat_report_task
 
 __all__ = [
@@ -10,4 +11,5 @@ __all__ = [
     "process_buffered_messages_task",
     "process_buffered_reactions_task",
     "process_buffered_replies_task",
+    "delete_welcome_message_task",
 ]
