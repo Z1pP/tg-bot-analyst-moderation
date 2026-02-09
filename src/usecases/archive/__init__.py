9650fc1fb73dc7ff2634101ab9ecc9c97945a55e
Z1pP <maxm_99@mail.ru>
2026-02-09 19:37:50 +0300

    Add NotifyArchiveChatNewMemberUseCase to the archive usecases module for handling new member notifications in archive chats.

--- src/usecases/archive/__init__.py
+++ src/usecases/archive/__init__.py
@@ -1,8 +1,10 @@
 """Archive usecases package."""
 
 from .get_archive_settings import ArchiveSettingsResult, GetArchiveSettingsUseCase
+from .notify_new_member import NotifyArchiveChatNewMemberUseCase
 
 __all__ = [
     "ArchiveSettingsResult",
     "GetArchiveSettingsUseCase",
+    "NotifyArchiveChatNewMemberUseCase",
 ]
