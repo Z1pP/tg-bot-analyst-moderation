7322863700140d74fa0cb5540f5c6035d2db70ad
Z1pP <maxm_99@mail.ru>
2026-02-10 00:17:38 +0300

    Refactor cancel_warn functionality to streamline success message handling. Consolidate response for canceling warnings into a single success message, enhancing clarity and reducing code complexity.

--- src/handlers/private/moderation/amnesty/cancel_warn.py
+++ src/handlers/private/moderation/amnesty/cancel_warn.py
@@ -3,10 +3,8 @@
 from punq import Container
 
 from constants import Dialog
-from constants.punishment import PunishmentType
 from dto import AmnestyUserDTO
 from usecases.amnesty import CancelLastWarnUseCase
-from utils.formatter import format_duration
 
 
 async def execute_cancel_warn(
@@ -26,23 +24,7 @@ async def execute_cancel_warn(
         AmnestyError: При ошибке выполнения (пробрасывается из use case).
     """
     usecase = container.resolve(CancelLastWarnUseCase)
-    result = await usecase.execute(dto=amnesty_dto)
-
-    if len(amnesty_dto.chat_dtos) == 1:
-        if result.next_punishment_type == PunishmentType.BAN:
-            next_step = "бессрочной блокировке."
-        elif result.next_punishment_type == PunishmentType.MUTE:
-            next_step = f"муту на {format_duration(result.next_punishment_duration)}"
-        else:
-            next_step = "предупреждению."
-
-        return Dialog.AmnestyUser.CANCEL_WARN_SUCCESS_SINGLE.format(
-            warn_count=result.current_warns_count,
-            username=amnesty_dto.violator_username,
-            next_step=next_step,
-        )
-
-    return Dialog.AmnestyUser.CANCEL_WARN_SUCCESS_ALL.format(
-        chats_count=len(amnesty_dto.chat_dtos),
-        username=amnesty_dto.violator_username,
+    await usecase.execute(dto=amnesty_dto)
+    return Dialog.AmnestyUser.CANCEL_WARN_SUCCESS.format(
+        username=amnesty_dto.violator_username
     )
