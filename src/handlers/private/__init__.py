a30d8dc6f8d9fb92d491fa4d3ba02dd2b24820c7
Z1pP <maxm_99@mail.ru>
2026-02-11 15:42:56 +0300

    Refactor private handlers to remove unused admin logs and roles routers, replacing them with the root menu router for improved organization and clarity.

--- src/handlers/private/__init__.py
+++ src/handlers/private/__init__.py
@@ -1,11 +1,5 @@
 from aiogram import Router
 
-from .admin_logs import (
-    pagination_router as admin_logs_pagination_router,
-)
-from .admin_logs import (
-    router as admin_logs_router,
-)
 from .analytics import router as analytics_router
 from .categories import router as categories_router
 from .chats import router as chats_router
@@ -13,9 +7,8 @@ from .common import router as common_router
 from .first_time_setup import router as first_time_setup_router
 from .message_management import router as message_management_router
 from .moderation import router as moderation_router
-from .release_notes import router as release_notes_router
 from .reports import router as reports_router
-from .roles import router as roles_router
+from .root_menu import router as root_menu_router
 from .scheduler import router as scheduler_router
 from .templates import router as templates_router
 from .users import router as users_router
@@ -30,9 +23,6 @@ router.include_router(reports_router)
 router.include_router(templates_router)
 router.include_router(categories_router)
 router.include_router(message_management_router)
-router.include_router(admin_logs_router)
-router.include_router(admin_logs_pagination_router)
-router.include_router(release_notes_router)
-router.include_router(roles_router)
+router.include_router(root_menu_router)
 router.include_router(scheduler_router)
 router.include_router(first_time_setup_router)
