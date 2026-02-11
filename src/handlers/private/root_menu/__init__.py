ee69a0719ff66effe0abc40b4c4ffdb474d2d42a
Z1pP <maxm_99@mail.ru>
2026-02-11 15:55:12 +0300

    Update root menu router to use RootDevOnlyFilter for message and callback query handling, enhancing user role enforcement for root and developer roles.

--- src/handlers/private/root_menu/__init__.py
+++ src/handlers/private/root_menu/__init__.py
@@ -1,11 +1,16 @@
 from aiogram import Router
 
+from filters.root_dev_filter import RootDevOnlyFilter
+
 from .admin_logs import router as admin_logs_router
 from .navigation import router as navigation_router
 from .release_notes import router as release_notes_router
 from .roles_manager import router as roles_manager_router
 
-router = Router(name="admin_menu_router")
+router = Router(name="root_menu_router")
+router.message.filter(RootDevOnlyFilter())
+router.callback_query.filter(RootDevOnlyFilter())
+
 
 router.include_router(navigation_router)
 router.include_router(admin_logs_router)
