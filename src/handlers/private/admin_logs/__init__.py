755c0cb9601f5ac13b1ad51ceb67e9ac56aa390a
Z1pP <maxm_99@mail.ru>
2026-02-11 15:42:24 +0300

    Remove

--- src/handlers/private/admin_logs/__init__.py
+++
@@ -1,4 +0,0 @@
-from .list_logs import router
-from .pagination import router as pagination_router
-
-__all__ = ["router", "pagination_router"]
--- src/handlers/private/roles/__init__.py
+++
@@ -1,6 +0,0 @@
-from aiogram import Router
-
-from .role_handler import router as role_router
-
-router = Router(name="roles_router")
-router.include_router(role_router)
