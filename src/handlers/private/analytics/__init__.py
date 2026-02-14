6d413e41be030b82aad3204e8d8c00633a2da418
Z1pP <maxm_99@mail.ru>
2026-02-14 21:10:43 +0300

    Update analytics router to include navigation module instead of menu for improved structure.

--- src/handlers/private/analytics/__init__.py
+++ src/handlers/private/analytics/__init__.py
@@ -1,8 +1,8 @@
 from aiogram import Router
 
-from .menu import router as menu_router
+from .navigation import router as navigation_router
 
 router = Router(name=__name__)
 
 
-router.include_router(menu_router)
+router.include_router(navigation_router)
