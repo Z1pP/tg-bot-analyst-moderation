5044f46c48aea6a4246bd4a54580f7f62ffdea37
Z1pP <maxm_99@mail.ru>
2026-02-14 20:41:15 +0300

    Update message management router to include navigation module instead of menu for improved structure.

--- src/handlers/private/message_management/__init__.py
+++ src/handlers/private/message_management/__init__.py
@@ -3,14 +3,14 @@ from aiogram import Router
 from .actions import router as actions_router
 from .delete import router as delete_router
 from .link import router as link_router
-from .menu import router as menu_router
+from .navigation import router as navigation_router
 from .pagination import router as pagination_router
 from .reply import router as reply_router
 from .send import router as send_router
 
 router = Router()
 
-router.include_router(menu_router)
+router.include_router(navigation_router)
 router.include_router(link_router)
 router.include_router(actions_router)
 router.include_router(delete_router)
