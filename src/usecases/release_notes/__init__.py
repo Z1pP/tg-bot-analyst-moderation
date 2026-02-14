c8e15059e5b7d1a4f931e86cb4f9597680300da8
Z1pP <maxm_99@mail.ru>
2026-02-14 20:15:46 +0300

    Update release notes use cases to include broadcasting text to admins and simplify exports by removing unused cases.

--- src/usecases/release_notes/__init__.py
+++ src/usecases/release_notes/__init__.py
@@ -1,19 +1,7 @@
-"""Use cases для релизных заметок."""
+"""Use cases для релизных заметок (рассылка текста владельцам и админам)."""
 
-from .broadcast_release_note import BroadcastReleaseNoteUseCase
-from .create_release_note import CreateReleaseNoteUseCase
-from .delete_release_note import DeleteReleaseNoteUseCase
-from .get_release_note import GetReleaseNoteUseCase
-from .get_release_notes_page import GetReleaseNotesPageUseCase
-from .update_release_note_content import UpdateReleaseNoteContentUseCase
-from .update_release_note_title import UpdateReleaseNoteTitleUseCase
+from .broadcast_text_to_admins import BroadcastTextToAdminsUseCase
 
 __all__ = [
-    "GetReleaseNotesPageUseCase",
-    "GetReleaseNoteUseCase",
-    "CreateReleaseNoteUseCase",
-    "UpdateReleaseNoteTitleUseCase",
-    "UpdateReleaseNoteContentUseCase",
-    "DeleteReleaseNoteUseCase",
-    "BroadcastReleaseNoteUseCase",
+    "BroadcastTextToAdminsUseCase",
 ]
