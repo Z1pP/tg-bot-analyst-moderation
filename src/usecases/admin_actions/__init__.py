ecd07feba5d1a51b93468e78cd94025912b74f27
Z1pP <maxm_99@mail.ru>
2026-02-15 14:52:51 +0300

    Add BroadcastMessageToTrackedChatsUseCase to admin actions module for improved message broadcasting capabilities.

--- src/usecases/admin_actions/__init__.py
+++ src/usecases/admin_actions/__init__.py
@@ -1,8 +1,10 @@
+from .broadcast_message_to_tracked_chats import BroadcastMessageToTrackedChatsUseCase
 from .delete_message import DeleteMessageUseCase
 from .reply_to_message import ReplyToMessageUseCase
 from .send_message_to_chat import SendMessageToChatUseCase
 
 __all__ = [
+    "BroadcastMessageToTrackedChatsUseCase",
     "DeleteMessageUseCase",
     "ReplyToMessageUseCase",
     "SendMessageToChatUseCase",
