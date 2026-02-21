eafc3503a1005b4d12c1f520119d1de67457968e
Z1pP <maxm_99@mail.ru>
2026-02-21 13:55:05 +0300

    Refactor execute_unmute to utilize 'format_violator_mention_suffix' for improved clarity in user identification.

--- src/handlers/private/moderation/amnesty/unmute.py
+++ src/handlers/private/moderation/amnesty/unmute.py
@@ -5,6 +5,7 @@ from punq import Container
 from constants import Dialog
 from dto import AmnestyUserDTO
 from usecases.amnesty import UnmuteUserUseCase
+from utils.moderation import format_violator_mention_suffix
 
 
 async def execute_unmute(
@@ -25,6 +26,7 @@ async def execute_unmute(
     """
     usecase = container.resolve(UnmuteUserUseCase)
     await usecase.execute(dto=amnesty_dto)
-    return Dialog.AmnestyUser.UNMUTE_SUCCESS.format(
-        username=amnesty_dto.violator_username
+    username_display = format_violator_mention_suffix(
+        amnesty_dto.violator_username, amnesty_dto.violator_tgid
     )
+    return Dialog.AmnestyUser.UNMUTE_SUCCESS.format(username=username_display)
