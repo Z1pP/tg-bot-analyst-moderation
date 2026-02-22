4bf2d39d4297304ccdd038dd0ff1af613c9d31ac
Z1pP <maxm_99@mail.ru>
2026-02-23 02:59:03 +0300

    Add error notification for scheduler task failures in taskiq_scheduler_entrypoint.py. Integrate BotMessageService and UserRepository to notify developers about exceptions during task execution, improving error handling and communication.

--- src/taskiq_scheduler_entrypoint.py
+++ src/taskiq_scheduler_entrypoint.py
@@ -2,8 +2,11 @@ import asyncio
 import logging
 
 from container import ContainerSetup, container
+from repositories import UserRepository
 from scheduler import broker
+from services import BotMessageService
 from services.scheduler import TaskiqSchedulerService
+from utils.exception_handler import notify_devs_about_error
 from utils.logger_config import setup_logger
 
 setup_logger(log_level=logging.INFO)
@@ -23,6 +26,12 @@ async def main():
                 await taskiq_service.tick()
             except Exception as e:
                 logger.error("Ошибка в тике scheduler: %s", e, exc_info=True)
+                await notify_devs_about_error(
+                    bot_message_service=container.resolve(BotMessageService),
+                    user_repository=container.resolve(UserRepository),
+                    exc=e,
+                    context="taskiq_scheduler_entrypoint: tick()",
+                )
 
             await asyncio.sleep(60)
     except asyncio.CancelledError:
