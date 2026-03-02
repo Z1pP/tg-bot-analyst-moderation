a5810e01abbe538e998d850628ffe95cd0d340a5
Z1pP <maxm_99@mail.ru>
2026-03-02 19:13:24 +0300

    Add NotifyArchiveChatMemberLeftUseCase to __all__ exports in archive module. This change ensures the use case is accessible for notifications related to members leaving group chats, maintaining consistency in the module's interface.

--- src/usecases/archive/__init__.py
+++ src/usecases/archive/__init__.py
@@ -3,6 +3,7 @@
 from .bind_archive_chat import BindArchiveChatUseCase
 from .generate_bind_hash import GenerateArchiveBindHashUseCase
 from .get_archive_settings import ArchiveSettingsResult, GetArchiveSettingsUseCase
+from .notify_member_left import NotifyArchiveChatMemberLeftUseCase
 from .notify_new_member import NotifyArchiveChatNewMemberUseCase
 from .set_archive_sending_time import (
     SetArchiveSendingTimeResult,
@@ -15,6 +16,7 @@ __all__ = [
     "BindArchiveChatUseCase",
     "GenerateArchiveBindHashUseCase",
     "GetArchiveSettingsUseCase",
+    "NotifyArchiveChatMemberLeftUseCase",
     "NotifyArchiveChatNewMemberUseCase",
     "SetArchiveSendingTimeResult",
     "SetArchiveSendingTimeUseCase",
