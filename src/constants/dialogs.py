7992f90b83da2bb4a3bdabc1be33d5cd65114daf
Z1pP <maxm_99@mail.ru>
2026-03-02 19:23:31 +0300

    Add MEMBER_KICKED dialog template to ArchiveNotificationDialogs for notifying when a member is kicked from a group. This template includes details such as username, ID, and timestamp, improving communication in chat scenarios.

--- src/constants/dialogs.py
+++ src/constants/dialogs.py
@@ -912,6 +912,14 @@ class ArchiveNotificationDialogs:
         "Чат: {chat_title}"
     )
 
+    MEMBER_KICKED = (
+        "👢 <b>Участник кикнут из группы</b>\n"
+        "Нейм: {username}\n"
+        "ID: <code>{tg_id}</code>\n"
+        "Когда: {date} {time}\n"
+        "Чат: {chat_title}"
+    )
+
 
 class ModerationReportDialogs:
     """Шаблоны для отчетов о наказаниях в архивный чат"""
