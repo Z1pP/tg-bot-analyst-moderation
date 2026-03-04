805e808fb4c7b39425afeb95f2ddf6b33f5a6ea2
Z1pP <maxm_99@mail.ru>
2026-03-04 15:47:54 +0300

    Update MEMBER_KICKED notification message for clarity and consistency in user experience.

--- src/constants/dialogs.py
+++ src/constants/dialogs.py
@@ -913,7 +913,7 @@ class ArchiveNotificationDialogs:
     )
 
     MEMBER_KICKED = (
-        "👢 <b>Участник кикнут из группы</b>\n"
+        "❌<b>Кик юзера</b> (🛡Антибот)\n"
         "Нейм: {username}\n"
         "ID: <code>{tg_id}</code>\n"
         "Когда: {date} {time}\n"
